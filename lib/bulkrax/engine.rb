5992a0dfaacb549cfb6ffe68da51993167980f27
Rob Kaufman <rob@notch8.com>
2019-06-22 08:54:11 -0700

    no longer have to install migrations, just run them

--- lib/bulkrax/engine.rb
+++ lib/bulkrax/engine.rb
@@ -3,6 +3,13 @@ require 'oai'
 module Bulkrax
   class Engine < ::Rails::Engine
     isolate_namespace Bulkrax
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
 
     config.generators do |g|
       g.test_framework :rspec
