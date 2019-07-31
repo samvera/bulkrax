b767c9a2f4596b1a25519d1d8cb641deb80173c0
Julie Allinson <julie@notch8.com>
2019-07-31 22:21:00 +0100

    consistency in lib/bulkrax.rb

--- lib/bulkrax.rb
+++ lib/bulkrax.rb
@@ -1,16 +1,16 @@
-require 'bulkrax/engine'
+require "bulkrax/engine"
 
 module Bulkrax
   class << self
     mattr_accessor :parsers, :system_identifier_field, :default_field_mapping, :reserved_properties, :field_mappings
 
     self.parsers = [
-      { name: 'OAI - Dublin Core', class_name: 'Bulkrax::OaiDcParser', partial: 'oai_fields' },
-      { name: 'OAI - Qualified Dublin Core', class_name: 'Bulkrax::OaiQualifiedDcParser', partial: 'oai_fields' },
-      { name: 'CSV - Comma Separated Values', class_name: 'Bulkrax::CsvParser', partial: 'csv_fields' }
+      { name: "OAI - Dublin Core", class_name: "Bulkrax::OaiDcParser", partial: "oai_fields" },
+      { name: "OAI - Qualified Dublin Core", class_name: "Bulkrax::OaiQualifiedDcParser", partial: "oai_fields" },
+      { name: "CSV - Comma Separated Values", class_name: "Bulkrax::CsvParser", partial: "csv_fields" }
     ]
 
-    self.system_identifier_field = 'source'
+    self.system_identifier_field = "source"
 
     # Hash of Generic field_mappings for use in the view
     # There must be one field_mappings hash per view parial
@@ -32,14 +32,14 @@ module Bulkrax
         "related_url" => { from: ["relation"] },
         "rights_statement" => { from: ["rights"] },
         "source" => { from: ["source"] },
-        "subject" => { from: ["subject"], parsed: true},
+        "subject" => { from: ["subject"], parsed: true },
         "title" => { from: ["title"] },
         "resource_type" => { from: ["type"], parsed: true },
-        "remote_files" => { from: ['thumbnail_url'], parsed: true }
+        "remote_files" => { from: ["thumbnail_url"], parsed: true }
       },
       "Bulkrax::OaiQualifiedDcParser" => {
         "abstract" => { from: ["abstract"] },
-        "alternative_title" => { from: ["alternative"]},
+        "alternative_title" => { from: ["alternative"] },
         "bibliographic_citation" => { from: ["bibliographicCitation"] },
         "contributor" => { from: ["contributor"] },
         "creator" => { from: ["creator"] },
@@ -55,7 +55,7 @@ module Bulkrax
         "subject" => { from: ["subject"], parsed: true },
         "title" => { from: ["title"] },
         "resource_type" => { from: ["type"], parsed: true },
-        "remote_files" => { from: ['thumbnail_url'], parsed: true }
+        "remote_files" => { from: ["thumbnail_url"], parsed: true }
       }
     }
 
