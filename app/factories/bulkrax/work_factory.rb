f0bb4b9fb2e8e7e7f820a1217ff0c3ba13be0e0d
Rob Kaufman <rob@notch8.com>
2019-09-26 02:07:14 -0700

    do not auto load workfactory if work model does not exist

--- app/factories/bulkrax/work_factory.rb
+++ app/factories/bulkrax/work_factory.rb
@@ -1,9 +1,11 @@
-module Bulkrax
-  class WorkFactory < ObjectFactory
-    include WithAssociatedCollection
+if defined?(Work)
+  module Bulkrax
+    class WorkFactory < ObjectFactory
+      include WithAssociatedCollection
 
-    self.klass = Work
-    # A way to identify objects that are not Hydra minted identifiers
-    self.system_identifier_field = Bulkrax.system_identifier_field
+      self.klass = Work
+      # A way to identify objects that are not Hydra minted identifiers
+      self.system_identifier_field = Bulkrax.system_identifier_field
+    end
   end
 end
