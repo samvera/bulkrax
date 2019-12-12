46c20984a00e56b36e0656fc19a614ae6db88979
Julie Allinson <julie@notch8.com>
2019-12-12 17:01:47 +0000

    add a migration for last_error

---
+++ db/migrate/20191212155530_change_entry_last_error.rb
@@ -0,0 +1,28 @@
+class ChangeEntryLastError < ActiveRecord::Migration[5.1]
+
+  class Bulkrax::Entry < ActiveRecord::Base
+  end
+
+  def change
+    errors = {}
+    last_error = Bulkrax::Entry.arel_table[:last_error]
+    Bulkrax::Entry.where(last_error.matches("%\n\n%")).each do | entry |
+      old_errors = entry.last_error.split("\n\n")
+      errors[entry.id] = { 
+        'error_class' => 'unknown', 
+        'error_message' => old_errors.first,
+        'error_trace' => old_errors.last
+      }
+      entry.update_attribute(:last_error, nil)
+    end
+
+    Bulkrax::Entry.class_eval do
+      serialize :last_error, JSON
+    end
+
+    errors.each_pair do | entry, value |
+      Bulkrax::Entry.find(entry).update_attribute(:last_error, value)
+    end 
+  end
+
+end
