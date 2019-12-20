ff9fffcc8b580d748afbf2ce18a574e835da58c1
Kiah <kiah@notch8.com>
2019-12-20 14:21:19 -0800

    resolve rubocop offenses for engine.rb

--- lib/bulkrax/engine.rb
+++ lib/bulkrax/engine.rb
@@ -6,7 +6,7 @@ module Bulkrax
   class Engine < ::Rails::Engine
     isolate_namespace Bulkrax
     initializer :append_migrations do |app|
-      if !app.root.to_s.match(root.to_s) && app.root.join('db/migrate').children.none? {|path| path.fnmatch?("*.bulkrax.rb")}
+      if !app.root.to_s.match(root.to_s) && app.root.join('db/migrate').children.none? { |path| path.fnmatch?("*.bulkrax.rb") }
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path
         end
@@ -15,18 +15,18 @@ module Bulkrax
 
     config.generators do |g|
       g.test_framework :rspec
-      g.fixture_replacement :factory_bot, :dir => 'spec/factories'
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
     end
 
     config.after_initialize do
       my_engine_root = Bulkrax::Engine.root.to_s
-      paths = ActionController::Base.view_paths.collect{|p| p.to_s}
+      paths = ActionController::Base.view_paths.collect(&:to_s)
       hyrax_path = paths.detect { |path| path.match('/hyrax-') }
-      if hyrax_path
-        paths = paths.insert(paths.index(hyrax_path), my_engine_root + '/app/views')
-      else
-        paths = paths.insert(0, my_engine_root + '/app/views')
-      end
+      paths = if hyrax_path
+                paths.insert(paths.index(hyrax_path), my_engine_root + '/app/views')
+              else
+                paths.insert(0, my_engine_root + '/app/views')
+              end
       ActionController::Base.view_paths = paths
     end
   end
