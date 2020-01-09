fd5b9f9e5e06085eb91732429e79b47344d94405
jpengstrom <jpengst@gmail.com>
2020-01-08 17:23:45 -0800

    saving

--- app/models/bulkrax/exporter_run.rb
+++ app/models/bulkrax/exporter_run.rb
@@ -10,7 +10,7 @@ module Bulkrax
       return "Failed" if export_runs&.processed_records == 0
       return "Complete" if export_runs&.enqueued_records == 0 && export_runs&.processed_records == export_runs&.total_work_entries
       return "Complete (with failures)" if export_runs&.failed_records > 0
-      return "Not yet exported" if export_runs&.total_work_entries == 0
+      return "Not yet exported" unless File.exist?(exporter.exporter_export_zip_path)
     end
 
   end
