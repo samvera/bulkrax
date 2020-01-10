2d4cde08313d34b5f88a210fb1ebe4de5a6278c5
Julie Allinson <julie@notch8.com>
2020-01-10 09:25:51 +0000

    change migration to up

--- db/migrate/20191212155530_change_entry_last_error.rb
+++ db/migrate/20191212155530_change_entry_last_error.rb
@@ -1,10 +1,10 @@
 class ChangeEntryLastError < ActiveRecord::Migration[5.1]
 
-  def change
+  def up
     # Use raw query to change text to JSON as last_error field is now serialized
     results = ActiveRecord::Base.connection.execute("SELECT id, last_error from bulkrax_entries WHERE last_error IS NOT null AND last_error LIKE '%\n\n%'")
     results.each do | error |
-      old_errors = error['last_error'].split("\n\n")
+      old_errors = error['last_error'].gsub("'","''").split("\n\n")
       new_error = {
         'error_class' => 'unknown', 
         'error_message' => old_errors.first,
@@ -13,5 +13,7 @@ class ChangeEntryLastError < ActiveRecord::Migration[5.1]
       ActiveRecord::Base.connection.execute("UPDATE bulkrax_entries SET last_error = '#{new_error.to_json}' WHERE id = '#{error['id']}'")
     end
   end
+
+  def down; end
 
 end
