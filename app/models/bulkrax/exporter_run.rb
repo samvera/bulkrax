264f4b86a14f7127300e9a9db402df7e9baf89fe
JP Engstrom <jpengst@gmail.com>
2020-01-17 04:15:51 -0800

    i58 exporter index page (#131)
    Exporter index page

--- app/models/bulkrax/exporter_run.rb
+++ app/models/bulkrax/exporter_run.rb
@@ -3,5 +3,15 @@
 module Bulkrax
   class ExporterRun < ApplicationRecord
     belongs_to :exporter
+
+    def exporter_status
+      export_runs = exporter.exporter_runs.last
+
+      return "Failed" if export_runs&.processed_records == 0
+      return "Complete" if export_runs&.enqueued_records == 0 && export_runs&.processed_records == export_runs&.total_work_entries
+      return "Complete (with failures)" if export_runs&.failed_records > 0
+      return "Not yet exported" unless File.exist?(exporter.exporter_export_zip_path) || export_runs&.total_work_entries == 0
+    end
+
   end
 end
