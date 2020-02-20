d940aca1307a5db134dbfa34ca892d7fa7a3d8b3
Rob Kaufman <rob@notch8.com>
2020-02-20 10:19:03 -0800

    fix controller inheritance to make multitenant work in hyku (#147)

--- app/controllers/bulkrax/application_controller.rb
+++ app/controllers/bulkrax/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Bulkrax
-  class ApplicationController < ActionController::Base
+  class ApplicationController < ::ApplicationController
     helper Rails.application.class.helpers
     protect_from_forgery with: :exception
   end
