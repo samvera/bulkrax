c684212c94e119cc8b5ba70c2ba44d89c246414d
Kiah <kiah@notch8.com>
2020-02-27 17:26:28 -0800

    xml entry spec, update related specs

---
+++ spec/models/bulkrax/xml_entry_spec.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module Bulkrax
+  RSpec.describe XmlEntry, type: :model do
+    let(:path) { './spec/fixtures/xml/good.xml' }
+    let(:data) { described_class.read_data(path) }
+
+    before do
+      Bulkrax.source_identifier_field_mapping = { 'Bulkrax::XmlEntry' => 'DrisUnique' }
+    end
+
+    describe 'class methods' do
+      context '#read_data' do
+        it 'reads the data from an xml file' do
+          expect(described_class.read_data(path)).to be_a(Nokogiri::XML::Document)
+        end
+      end
+
+      context '#data_for_entry' do
+        it 'retrieves the data and constructs a hash' do
+          expect(described_class.data_for_entry(data)).to eq(
+            source_identifier: '0145514',
+            data: "<!-- This grammar has been deprecated - use FMPXMLRESULT instead --><FMPDSORESULT>  <ROW MODID=\"12\" RECORDID=\"148142\">    <TitleLargerEntity>Test XML</TitleLargerEntity>    <Abstract>Lorem ipsum dolor sit amet.</Abstract>    <DrisUnique>0145514</DrisUnique>  </ROW></FMPDSORESULT>",
+            collection: [],
+            file: [],
+            children: []
+          )
+        end
+      end
+    end
+
+    describe '#build' do
+      subject { described_class.new(importerexporter: importer) }
+      let(:raw_metadata) { described_class.data_for_entry(data) }
+      let(:importer) { FactoryBot.build(:bulkrax_importer_xml) }
+
+      before do
+        Bulkrax.default_work_type = 'Work'
+        Bulkrax.field_mappings.merge!({
+          'Bulkrax::XmlParser' => {
+            'title' => { from: ['TitleLargerEntity'] },
+            'abstract' => { from: ['Abstract'] }
+          }
+        })
+      end
+
+      context 'with raw_metadata' do
+        before do
+          subject.raw_metadata = raw_metadata
+          allow_any_instance_of(ObjectFactory).to receive(:run).and_return(instance_of(Work))
+          allow_any_instance_of(User).to receive(:batch_user)
+        end
+
+        it 'succeeds' do
+          subject.build
+          expect(subject.status).to eq('succeeded')
+        end
+
+        it 'builds entry' do
+          subject.build
+          expect(subject.parsed_metadata).to eq("file" => [], "rights_statement" => [nil], "source" => ["0145514"], "title" => ["Test XML"], "visibility" => "open")
+        end
+      end
+
+      context 'without raw_metadata' do
+        before do
+          subject.raw_metadata = nil
+        end
+
+        it 'fails' do
+          subject.build
+          expect(subject.status).to eq('failed')
+        end
+      end
+    end
+  end
+end
