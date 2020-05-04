fc06a85ead75f05dc487ca4884581e80e13c0b55
Rob Kaufman <rob@notch8.com>
2020-05-04 15:39:22 -0700

    was having issues with relating works to each other (#194)
    * was having issues with relating works to each other, ended up doing some lite refactoring to find the bug
    
    * fix comment space

--- app/jobs/bulkrax/child_relationships_job.rb
+++ app/jobs/bulkrax/child_relationships_job.rb
@@ -6,87 +6,106 @@ module Bulkrax
     queue_as :import
 
     def perform(*args)
-      @entry = Entry.find(args[0])
-      @child_entries = args[1].map { |e| Entry.find(e) }
-      @child_works_hash = build_child_works_hash
-      @importer_run_id = args[2]
-      @user = @entry.importerexporter.user
+      @args = args
 
-      if @entry.factory_class == Collection
-        # add collection to works
-        member_of_collection = []
-        @child_works_hash.each_pair { |k, v| member_of_collection << k if v[:class_name] != 'Collection' }
-        member_of_collection.each { |work| work_child_collection_parent(work) }
-
-        # add collections to collection
-        members_collections = []
-        @child_works_hash.each_pair { |k, v| members_collections << k if v[:class_name] == 'Collection' }
-        collection_parent_collection_child(members_collections) unless members_collections.blank?
+      if entry.factory_class == Collection
+        collection_membership
       else
-        # add works to work
-        members_works = []
-        # reject any Collections, they can't be children of Works
-        @child_works_hash.each_pair { |k, v| members_works << k if v[:class_name] != 'Collection' }
-        if members_works.length < @child_entries.length # rubocop:disable Style/IfUnlessModifier
-          Rails.logger.warn("Cannot add collections as children of works: #{(@child_entries.length - members_works.length)} collections were discarded for parent entry #{@entry.id} (of #{@child_entries.length})")
-        end
-        work_parent_work_child(members_works) unless members_works.blank?
+        work_membership
       end
     # Not all of the Works/Collections exist yet; reschedule
     rescue Bulkrax::ChildWorksError
       reschedule(args[0], args[1], args[2])
     end
 
-    private
+    def collection_membership
+      # add collection to works
+      member_of_collection = []
+      child_works_hash.each { |k, v| member_of_collection << k if v[:class_name] != 'Collection' }
+      member_of_collection.each { |work| work_child_collection_parent(work) }
 
-      def build_child_works_hash
-        hash = {}
-        @child_entries.each do |child_entry|
-          work = child_entry.factory.find
-          # If we can't find the Work/Collection, raise a custom error
-          raise ChildWorksError if work.blank?
-          hash[work.id] = { class_name: work.class.to_s, source_identifier: child_entry.identifier }
-        end
-        return hash
+      # add collections to collection
+      members_collections = []
+      child_works_hash.each { |k, v| members_collections << k if v[:class_name] == 'Collection' }
+      collection_parent_collection_child(members_collections) if members_collections.present?
+    end
+
+    def work_membership
+      # add works to work
+      # reject any Collections, they can't be children of Works
+      members_works = []
+      # reject any Collections, they can't be children of Works
+      child_works_hash.each { |k, v| members_works << k if v[:class_name] != 'Collection' }
+      if members_works.length < child_entries.length # rubocop:disable Style/IfUnlessModifier
+        Rails.logger.warn("Cannot add collections as children of works: #{(@child_entries.length - members_works.length)} collections were discarded for parent entry #{@entry.id} (of #{@child_entries.length})")
       end
+      work_parent_work_child(members_works) if members_works.present?
+    end
+
+    def entry
+      @entry ||= Bulkrax::Entry.find(@args[0])
+    end
+
+    def child_entries
+      @child_entries ||= @args[1].map { |e| Bulkrax::Entry.find(e) }
+    end
+
+    def child_works_hash
+      @child_works_hash ||= child_entries.each_with_object({}) do |child_entry, hash|
+        work = child_entry.factory.find
+        # If we can't find the Work/Collection, raise a custom error
+        raise ChildWorksError if work.blank?
+        hash[work.id] = { class_name: work.class.to_s, source_identifier: child_entry.identifier }
+      end
+    end
+
+    def importer_run_id
+      @args[2]
+    end
+
+    def user
+      @user ||= entry.importerexporter.user
+    end
+
+    private
 
       # rubocop:disable Rails/SkipsModelValidations
       # Work-Collection membership is added to the child as member_of_collection_ids
       # This is adding the reverse relatinship, from the child to the parent
       def work_child_collection_parent(work_id)
-        attrs = { id: work_id, collections: [{ id: @entry.factory.find.id }] }
-        Bulkrax::ObjectFactory.new(attrs, @child_works_hash[work_id][:source_identifier], false, @user, @child_works_hash[work_id][:class_name].constantize).run
-        ImporterRun.find(@importer_run_id).increment!(:processed_children)
+        attrs = { id: work_id, collections: [{ id: entry.factory.find.id }] }
+        Bulkrax::ObjectFactory.new(attrs, child_works_hash[work_id][:source_identifier], false, user, child_works_hash[work_id][:class_name].constantize).run
+        ImporterRun.find(importer_run_id).increment!(:processed_children)
       rescue StandardError => e
-        @entry.status_info(e)
-        @entry.save!
-        ImporterRun.find(@importer_run_id).increment!(:failed_children)
+        entry.status_info(e)
+        entry.save!
+        ImporterRun.find(importer_run_id).increment!(:failed_children)
       end
 
       # Collection-Collection membership is added to the as member_ids
       def collection_parent_collection_child(member_ids)
-        attrs = { id: @entry.factory.find.id, children: member_ids }
-        Bulkrax::ObjectFactory.new(attrs, @entry.identifier, false, @user, @entry.factory_class).run
-        ImporterRun.find(@importer_run_id).increment!(:processed_children)
+        attrs = { id: entry.factory.find.id, children: member_ids }
+        Bulkrax::ObjectFactory.new(attrs, entry.identifier, false, user, entry.factory_class).run
+        ImporterRun.find(importer_run_id).increment!(:processed_children)
       rescue StandardError => e
-        @entry.status_info(e)
-        @entry.save!
-        ImporterRun.find(@importer_run_id).increment!(:failed_children)
+        entry.status_info(e)
+        entry.save!
+        ImporterRun.find(importer_run_id).increment!(:failed_children)
       end
 
       # Work-Work membership is added to the parent as member_ids
       def work_parent_work_child(member_ids)
         # build work_members_attributes
-        attrs = { id: @entry.factory.find.id,
+        attrs = { id: entry.factory.find.id,
                   work_members_attributes: member_ids.each.with_index.each_with_object({}) do |(member, index), ids|
-                                             ids[index] = { id: member }
-                                           end }
-        Bulkrax::ObjectFactory.new(attrs, @entry.identifier, false, @user, @entry.factory_class).run
-        ImporterRun.find(@importer_run_id).increment!(:processed_children)
+                    ids[index] = { id: member }
+                  end }
+        Bulkrax::ObjectFactory.new(attrs, entry.identifier, false, user, entry.factory_class).run
+        ImporterRun.find(importer_run_id).increment!(:processed_children)
       rescue StandardError => e
-        @entry.status_info(e)
-        @entry.save!
-        ImporterRun.find(@importer_run_id).increment!(:failed_children)
+        entry.status_info(e)
+        entry.save!
+        ImporterRun.find(importer_run_id).increment!(:failed_children)
       end
       # rubocop:enable Rails/SkipsModelValidations
 
