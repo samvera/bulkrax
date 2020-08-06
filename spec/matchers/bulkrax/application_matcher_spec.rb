23ce3734a248f8180be44ba6567214bbc347ce87
Rob Kaufman <rob@notch8.com>
2020-08-05 17:21:41 -0700

    rubocop

--- spec/matchers/bulkrax/application_matcher_spec.rb
+++ spec/matchers/bulkrax/application_matcher_spec.rb
@@ -5,31 +5,29 @@ require 'rails_helper'
 module Bulkrax
   RSpec.describe ApplicationMatcher do
     describe 'handling the split argument' do
-
       it 'default split' do
         matcher = ApplicationMatcher.new(split: true)
         result = matcher.result(nil, " hey ; how : are | you")
-        expect(result).to eq(["hey", "how", "are", "you"]) 
+        expect(result).to eq(["hey", "how", "are", "you"])
       end
 
       it 'custom regex split' do
         matcher = ApplicationMatcher.new(split: /\s*[;]\s*/)
         result = matcher.result(nil, " hey ; how : are | you")
-        expect(result).to eq(["hey", "how : are | you"]) 
+        expect(result).to eq(["hey", "how : are | you"])
       end
 
       it 'no split' do
         matcher = ApplicationMatcher.new(split: false)
         result = matcher.result(nil, " hey ; how : are | you")
-        expect(result).to eq("hey ; how : are | you") 
+        expect(result).to eq("hey ; how : are | you")
       end
 
       it 'custom split' do
         matcher = ApplicationMatcher.new(split: '\|')
         result = matcher.result(nil, " hey ; how : are | you")
-        expect(result).to eq(["hey ; how : are", "you"]) 
+        expect(result).to eq(["hey ; how : are", "you"])
       end
-
     end
   end
 end
