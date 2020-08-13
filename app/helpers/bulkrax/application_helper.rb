b3b04c5d6e098f61fbd2e634d6bd4b1811b7dfb6
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2020-08-12 21:24:42 -0700

    Require CodeRay in application_controller so all views can access it (#229)
    * require coderay in application_controller so all views can access it
    
    * move CodeRay calls to helper
    
    * fix rubocop
    
    * remove old fix

--- app/helpers/bulkrax/application_helper.rb
+++ app/helpers/bulkrax/application_helper.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
+require 'coderay'
+
 module Bulkrax
   module ApplicationHelper
     include Hyrax::HyraxHelperBehavior
+
+    def coderay(value, opts)
+      CodeRay
+        .scan(value, :ruby)
+        .html(opts)
+        .html_safe # rubocop:disable Rails/OutputSafety
+    end
   end
 end
