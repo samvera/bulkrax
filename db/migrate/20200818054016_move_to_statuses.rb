b2ce412ac2bf61d88174c1e981ab5ec74c5aa09b
Rob Kaufman <rob@notch8.com>
2020-09-13 21:57:11 -0700

    add migration of last status

--- db/migrate/20200818054016_move_to_statuses.rb
+++
@@ -1,4 +0,0 @@
-class MoveToStatuses < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
--- spec/test_app/db/schema.rb
+++ spec/test_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200818055819) do
+ActiveRecord::Schema.define(version: 20200819054016) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id", null: false
