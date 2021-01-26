6f2f20a60387495bc142e5f57d62d3675452425d
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2021-01-26 09:14:34 -0800

    Add guards to new migrations (#266)
    * add guards to new migrations
    
    * fix indentation to appease rubocop

--- db/migrate/20201106014204_add_date_filter_and_status_to_bulkrax_exporters.rb
+++ db/migrate/20201106014204_add_date_filter_and_status_to_bulkrax_exporters.rb
@@ -1,7 +1,7 @@
 class AddDateFilterAndStatusToBulkraxExporters < ActiveRecord::Migration[5.1]
   def change
-    add_column :bulkrax_exporters, :start_date, :date
-    add_column :bulkrax_exporters, :finish_date, :date
-    add_column :bulkrax_exporters, :work_visibility, :string
+    add_column :bulkrax_exporters, :start_date, :date unless column_exists?(:bulkrax_exporters, :start_date)
+    add_column :bulkrax_exporters, :finish_date, :date unless column_exists?(:bulkrax_exporters, :finish_date)
+    add_column :bulkrax_exporters, :work_visibility, :string unless column_exists?(:bulkrax_exporters, :work_visibility)
   end
 end
