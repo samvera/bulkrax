3ab20bd7fc070382fec8b23458ca77c466c8e4a7
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2021-03-01 10:55:00 -0800

    always create a PermissionTemplateAccess
    this replaces using manage_users on PermissionTemplate creation, which is invalid
    
    Co-authored-by: Daniel Pierce <dlpierce@indiana.edu>

--- app/jobs/bulkrax/import_work_collection_job.rb
+++ app/jobs/bulkrax/import_work_collection_job.rb
@@ -26,18 +26,15 @@ module Bulkrax
       def add_user_to_permission_template!(entry)
         user                = ::User.find(entry.importerexporter.user_id)
         collection          = entry.factory.find
-        permission_template = Hyrax::PermissionTemplate.find_by(source_id: collection.id)
-
-        if permission_template.present?
-          Hyrax::PermissionTemplateAccess.create!(
-            permission_template_id: permission_template.id,
-            agent_id: user.user_key,
-            agent_type: 'user',
-            access: 'manage'
-          )
-        else
-          Hyrax::PermissionTemplate.create!(source_id: collection.id, manage_users: [user])
-        end
+        permission_template = Hyrax::PermissionTemplate.find_or_create_by!(source_id: collection.id)
+        
+        Hyrax::PermissionTemplateAccess.create!(
+          permission_template_id: permission_template.id,
+          agent_id: user.user_key,
+          agent_type: 'user',
+          access: 'manage'
+        )
+        
 
         collection.reset_access_controls!
       end
