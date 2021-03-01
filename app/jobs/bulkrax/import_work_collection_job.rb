eb744730941774030b7f3125ab7e759a4359a2fb
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2021-03-01 10:58:10 -0800

    fix trailing whitespace

--- app/jobs/bulkrax/import_work_collection_job.rb
+++ app/jobs/bulkrax/import_work_collection_job.rb
@@ -27,14 +27,13 @@ module Bulkrax
         user                = ::User.find(entry.importerexporter.user_id)
         collection          = entry.factory.find
         permission_template = Hyrax::PermissionTemplate.find_or_create_by!(source_id: collection.id)
-        
+
         Hyrax::PermissionTemplateAccess.create!(
           permission_template_id: permission_template.id,
           agent_id: user.user_key,
           agent_type: 'user',
           access: 'manage'
         )
-        
 
         collection.reset_access_controls!
       end
