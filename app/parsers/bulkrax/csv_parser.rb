2bd5bb6a44c4578de6b44a6a0f127e341dc0b642
Alisha Evans <alishaevn2@gmail.com>
2021-07-29 17:47:07 -0700

    simplify-source-identifying (#298)
    * set the source_identifier_key
    
    * change source identifiers back to a string
    
    * update specs to handle numerated csv headers without a numerated key
    
    * able to set the source_identifier as a key AND string
    
    * update field_to to properly handle arrays and strings
    
    * address rubocop offense

--- app/parsers/bulkrax/csv_parser.rb
+++ app/parsers/bulkrax/csv_parser.rb
@@ -38,7 +38,7 @@ module Bulkrax
     end
 
     def required_elements
-      %w[title source_identifier]
+      ['title', source_identifier_key]
     end
 
     def valid_import?
@@ -63,9 +63,17 @@ module Bulkrax
       end
     end
 
+    def source_identifier_key
+      @source_identifier_key ||= entry_class.source_identifier_field
+    end
+
+    def source_identifier_symbol
+      @source_identifier_symbol ||= source_identifier_key.to_sym
+    end
+
     def create_works
       records.each_with_index do |record, index|
-        if record[:source_identifier].blank?
+        if record[source_identifier_symbol].blank?
           current_run.invalid_records ||= ""
           current_run.invalid_records += "Missing #{Bulkrax.system_identifier_field} for #{record.to_h}\n"
           current_run.failed_records += 1
@@ -74,8 +82,8 @@ module Bulkrax
         end
         break if limit_reached?(limit, index)
 
-        seen[record[:source_identifier]] = true
-        new_entry = find_or_create_entry(entry_class, record[:source_identifier], 'Bulkrax::Importer', record.to_h.compact)
+        seen[record[source_identifier_symbol]] = true
+        new_entry = find_or_create_entry(entry_class, record[source_identifier_symbol], 'Bulkrax::Importer', record.to_h.compact)
         if record[:delete].present?
           DeleteWorkJob.send(perform_method, new_entry, current_run)
         else
