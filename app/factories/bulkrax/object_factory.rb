28d986c5c963aea6f51e54a260269ba09c23b02e
Braydon Justice <sephirothkod@users.noreply.github.com>
2021-09-07 21:32:50 -0700

    fixes hash having integers as keys on create (#338)
    * fixes hash having integers as keys issue
    
    * adds check for hash object when creating attributes
    
    * better handling of hashes in collection attributes
    
    * refactor collection attributes to be handled before being passed into the ObjectFactory
    
    * lint fix...
    
    * add member_of_collections_attributes to permitted attributes

--- app/factories/bulkrax/object_factory.rb
+++ app/factories/bulkrax/object_factory.rb
@@ -192,32 +192,16 @@ module Bulkrax
     # which is used by Hyrax::Actors::AddAsMemberOfCollectionsActor
     def create_attributes
       return transform_attributes if klass == Collection
-      if attributes[:collection].present?
-        transform_attributes.except(:collection).merge(member_of_collections_attributes: { 0 => { id: collection.id } })
-      elsif attributes[:collections].present?
-        collection_ids = attributes[:collections].each.with_index.each_with_object({}) do |(element, index), ids|
-          ids[index] = { id: element }
-        end
-        transform_attributes.except(:collections).merge(member_of_collections_attributes: collection_ids)
-      else
-        transform_attributes
-      end
+      ActiveSupport::Deprecation.warn("Passing collection or collections directly to the ObjectFactory is no longer supported. Please update your Entry class to call add_collections instead.") if attributes[:collection].present? || attributes[:collections].present?
+      transform_attributes.except(:collections, :collection)
     end
 
     # Strip out the :collection key, and add the member_of_collection_ids,
     # which is used by Hyrax::Actors::AddAsMemberOfCollectionsActor
     def attribute_update
       return transform_attributes.except(:id) if klass == Collection
-      if attributes[:collection].present?
-        transform_attributes.except(:id).except(:collection).merge(member_of_collections_attributes: { "0" => { id: collection.id } })
-      elsif attributes[:collections].present?
-        collection_ids = attributes[:collections].each.with_index.each_with_object({}) do |(element, index), ids|
-          ids[index.to_s] = element
-        end
-        transform_attributes.except(:id).except(:collections).merge(member_of_collections_attributes: collection_ids)
-      else
-        transform_attributes.except(:id)
-      end
+      ActiveSupport::Deprecation.warn("Passing collection or collections directly to the ObjectFactory is no longer supported. Please update your Entry class to call add_collections instead.") if attributes[:collection].present? || attributes[:collections].present?
+      transform_attributes.except(:collections, :collection, :id)
     end
 
     # Override if we need to map the attributes from the parser in
@@ -230,7 +214,7 @@ module Bulkrax
 
     # Regardless of what the Parser gives us, these are the properties we are prepared to accept.
     def permitted_attributes
-      klass.properties.keys.map(&:to_sym) + %i[id edit_users edit_groups read_groups visibility work_members_attributes admin_set_id]
+      klass.properties.keys.map(&:to_sym) + %i[id edit_users edit_groups read_groups visibility work_members_attributes admin_set_id member_of_collections_attributes]
     end
   end
 end
