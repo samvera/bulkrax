8b2c83553a455ab54a249dfc976796d50942b94a
Rob Kaufman <rob@notch8.com>
2021-11-11 22:09:35 -0800

    do not set model to nil on raw csv data

--- app/models/bulkrax/csv_entry.rb
+++ app/models/bulkrax/csv_entry.rb
@@ -24,7 +24,7 @@ module Bulkrax
       data = data.first if data.is_a?(CSV::Table)
       # model has to be separated so that it doesn't get mistranslated by to_h
       raw_data = data.to_h
-      raw_data[:model] = data[:model]
+      raw_data[:model] = data[:model] if data[:model].present?
       # If the collection field mapping is not 'collection', add 'collection' - the parser needs it
       raw_data[:collection] = raw_data[collection_field.to_sym] if raw_data.keys.include?(collection_field.to_sym) && collection_field != 'collection'
       # If the children field mapping is not 'children', add 'children' - the parser needs it
