20b4253314e52ddbabc941d759d91646cd4b4af7
Rob Kaufman <rob@notch8.com>
2021-11-21 16:51:12 -0800

    protect exporter from halting if one record is not correct (#380)

--- app/parsers/bulkrax/csv_parser.rb
+++ app/parsers/bulkrax/csv_parser.rb
@@ -158,8 +158,11 @@ module Bulkrax
       current_work_ids.each_with_index do |wid, index|
         break if limit_reached?(limit, index)
         new_entry = find_or_create_entry(entry_class, wid, 'Bulkrax::Exporter')
-        entry = Bulkrax::ExportWorkJob.perform_now(new_entry.id, current_run.id)
-
+        begin
+          entry = Bulkrax::ExportWorkJob.perform_now(new_entry.id, current_run.id)
+        rescue => e
+          Rails.logger.info("#{e.message} was detected during export")
+        end
         self.headers |= entry.parsed_metadata.keys if entry
       end
     end
