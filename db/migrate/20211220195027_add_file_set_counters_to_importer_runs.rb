3ed2bbe793c80edb64ab9cdad75d1cdaa29c2daa
Benjamin Kiah Stroud <kiah@notch8.com>
2021-12-22 10:30:03 -0800

    guard migrations against "duplicate column name" errors

--- db/migrate/20211220195027_add_file_set_counters_to_importer_runs.rb
+++ db/migrate/20211220195027_add_file_set_counters_to_importer_runs.rb
@@ -1,6 +1,6 @@
 class AddFileSetCountersToImporterRuns < ActiveRecord::Migration[5.2]
   def change
-    add_column :bulkrax_importer_runs, :processed_file_sets, :integer, default: 0
-    add_column :bulkrax_importer_runs, :failed_file_sets, :integer, default: 0
+    add_column :bulkrax_importer_runs, :processed_file_sets, :integer, default: 0 unless column_exists?(:bulkrax_importer_runs, :processed_file_sets)
+    add_column :bulkrax_importer_runs, :failed_file_sets, :integer, default: 0 unless column_exists?(:bulkrax_importer_runs, :failed_file_sets)
   end
 end
