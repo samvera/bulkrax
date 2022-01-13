3bd2622b029e95237b4a2c42e018cf84dab6c63d
braydonjustice <braydonjustice@gmail.com>
2022-01-13 14:19:35 -0800

    temp to re run tests

--- app/jobs/bulkrax/delete_work_job.rb
+++ app/jobs/bulkrax/delete_work_job.rb
@@ -10,7 +10,7 @@ module Bulkrax
       work&.delete
       ImporterRun.find(importer_run.id).increment!(:deleted_records)
       ImporterRun.find(importer_run.id).decrement!(:enqueued_records)
-      entry.save!
+      entry.save
       entry.importer.current_run = ImporterRun.find(importer_run.id)
       entry.importer.record_status
       entry.status_info("Deleted", ImporterRun.find(importer_run.id))
