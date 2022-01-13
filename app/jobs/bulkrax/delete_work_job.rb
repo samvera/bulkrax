9df2942e14c831c0a94631a933d335d921b926df
braydonjustice <braydonjustice@gmail.com>
2022-01-13 14:27:01 -0800

    fallback to importer_run argument in delete_work_job.rb if no ID exists

--- app/jobs/bulkrax/delete_work_job.rb
+++ app/jobs/bulkrax/delete_work_job.rb
@@ -8,12 +8,12 @@ module Bulkrax
     def perform(entry, importer_run)
       work = entry.factory.find
       work&.delete
-      ImporterRun.find(importer_run.id).increment!(:deleted_records)
-      ImporterRun.find(importer_run.id).decrement!(:enqueued_records)
-      entry.save
-      entry.importer.current_run = ImporterRun.find(importer_run.id)
+      ImporterRun.find(importer_run.id).increment!(:deleted_records) || importer_run
+      ImporterRun.find(importer_run.id).decrement!(:enqueued_records) || importer_run
+      entry.save!
+      entry.importer.current_run = ImporterRun.find(importer_run.id) || importer_run
       entry.importer.record_status
-      entry.status_info("Deleted", ImporterRun.find(importer_run.id))
+      entry.status_info("Deleted", ImporterRun.find(importer_run.id) || importer_run)
     end
     # rubocop:enable Rails/SkipsModelValidations
   end
