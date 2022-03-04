320b8df263cf69f35efefdab402f684c1263b6da
braydonjustice <braydonjustice@gmail.com>
2022-03-04 11:23:10 -0800

    fix issues with array migration again

--- db/migrate/20220301020307_add_parents_to_bulkrax_importer_runs.rb
+++ db/migrate/20220301020307_add_parents_to_bulkrax_importer_runs.rb
@@ -1,4 +1,4 @@
-class AddParentsToBulkraxImporterRuns < ActiveRecord::Migration[5.2]
+class AddParentsToBulkraxImporterRuns < ActiveRecord::Migration[5.1]
   def change
     add_column :bulkrax_importer_runs, :parents, :text, default: [], array: true
   end
