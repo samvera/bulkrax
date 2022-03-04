fb18dfc4519bec4c7d2ef716319389fce6cfeb6b
braydonjustice <braydonjustice@gmail.com>
2022-03-03 17:18:01 -0800

    fix issues with arry in db in ruby 2.6

--- app/models/bulkrax/pending_relationship.rb
+++ app/models/bulkrax/pending_relationship.rb
@@ -3,5 +3,6 @@
 module Bulkrax
   class PendingRelationship < ApplicationRecord
     belongs_to :bulkrax_importer_run
+    serialize :parents
   end
 end
--- db/migrate/20220301020307_add_parents_to_bulkrax_importer_runs.rb
+++ db/migrate/20220301020307_add_parents_to_bulkrax_importer_runs.rb
@@ -1,5 +1,5 @@
 class AddParentsToBulkraxImporterRuns < ActiveRecord::Migration[5.2]
   def change
-    add_column :bulkrax_importer_runs, :parents, :string, array: true, default: []
+    add_column :bulkrax_importer_runs, :parents, :text, default: [], array: true
   end
 end
