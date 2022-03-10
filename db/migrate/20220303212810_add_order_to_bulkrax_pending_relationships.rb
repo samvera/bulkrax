6655321c38dd1c7b4693f8f3c058fae4fc3a31f8
braydonjustice <braydonjustice@gmail.com>
2022-03-10 11:45:24 -0800

    add defaults to table modifications

--- db/migrate/20220303212810_add_order_to_bulkrax_pending_relationships.rb
+++ db/migrate/20220303212810_add_order_to_bulkrax_pending_relationships.rb
@@ -1,5 +1,5 @@
 class AddOrderToBulkraxPendingRelationships < ActiveRecord::Migration[5.2]
   def change
-    add_column :bulkrax_pending_relationships, :order, :integer
+    add_column :bulkrax_pending_relationships, :order, :integer, default: 0
   end
 end
