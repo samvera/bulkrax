ce9df26b3d05c39e5ab505a6f2d3ba54ce5ff41a
Benjamin Kiah Stroud <kiah@notch8.com>
2022-03-17 09:38:42 -0700

    unit tests for #build_relationship_metadata

--- app/models/bulkrax/csv_entry.rb
+++ app/models/bulkrax/csv_entry.rb
@@ -124,6 +124,7 @@ module Bulkrax
     end
 
     def build_relationship_metadata
+      # Includes all relationship methods for all exportable record types (works, Collections, FileSets)
       relationship_methods = {
         related_parents_parsed_mapping => %i[member_of_collection_ids member_of_work_ids in_work_ids],
         related_children_parsed_mapping => %i[member_collection_ids member_work_ids file_set_ids]
