f1afa58515f1fd963dc927b570a4fab1e607aa3c
Alisha Evans <alisha@notch8.com>
2022-03-17 17:16:12 -0500

    remove parents column serialization in the importer run

--- app/models/bulkrax/importer_run.rb
+++ app/models/bulkrax/importer_run.rb
@@ -4,6 +4,6 @@ module Bulkrax
   class ImporterRun < ApplicationRecord
     belongs_to :importer
     has_many :statuses, as: :runnable, dependent: :destroy
-    serialize :parents, Array
+    # serialize :parents, Array
   end
 end
