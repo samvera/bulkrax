4325543cebb39e99eeea502e67fb27843c22fd56
Benjamin Kiah Stroud <kiah@notch8.com>
2022-03-30 10:11:43 -0700

    remove "array: true" from ImporterRun#parents in favor of #serialize to support more db types

--- spec/test_app/db/schema.rb
+++ spec/test_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_212810) do
+ActiveRecord::Schema.define(version: 2022_03_30_165510) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id", null: false
