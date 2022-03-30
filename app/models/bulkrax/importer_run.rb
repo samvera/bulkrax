5b95e0b7f7093e749fc657f3ef22c78703de365f
Benjamin Kiah Stroud <kiah@notch8.com>
2022-03-29 17:58:29 -0700

    serialize ImporterRun#parents as an Array

--- app/models/bulkrax/importer_run.rb
+++ app/models/bulkrax/importer_run.rb
@@ -4,5 +4,7 @@ module Bulkrax
   class ImporterRun < ApplicationRecord
     belongs_to :importer
     has_many :statuses, as: :runnable, dependent: :destroy
+
+    serialize :parents, Array
   end
 end
