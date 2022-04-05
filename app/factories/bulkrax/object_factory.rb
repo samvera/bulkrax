4f06ada73024162f73faf8382fb2c277d13747ba
Alisha Evans <alisha@notch8.com>
2022-04-05 17:20:46 -0500

    rubocop fix

--- app/factories/bulkrax/object_factory.rb
+++ app/factories/bulkrax/object_factory.rb
@@ -205,7 +205,7 @@ module Bulkrax
     def collection_type(attrs)
       # avoid the "ArgumentError: Identifier must be a string of size > 0 in order to be treeified" error
       # when setting object.attributes
-      attrs.delete('id') unless attrs['id'].present?
+      attrs.delete('id') if attrs['id'].blank?
       return attrs if attrs['collection_type_gid'].present?
 
       attrs['collection_type_gid'] = Hyrax::CollectionType.find_or_create_default_collection_type.gid
