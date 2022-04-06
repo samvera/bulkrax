09fb636360b8f5434f95b0f289ee5f6030b7a063
Alisha Evans <alisha@notch8.com>
2022-04-06 14:38:51 -0500

    add a conditional to find_record

--- app/models/concerns/bulkrax/dynamic_record_lookup.rb
+++ app/models/concerns/bulkrax/dynamic_record_lookup.rb
@@ -8,10 +8,16 @@ module Bulkrax
     # @param identifier [String] Work/Collection ID or Bulkrax::Entry source_identifier
     # @return [Work, Collection, nil] Work or Collection if found, otherwise nil
     def find_record(identifier, importer_run_id)
-      # account for the possibility that the same record may have successfully or unsuccessfully
-      # been imported in a different importer
-      importer_id = ImporterRun.find(importer_run_id).importer_id
-      record = Entry.find_by(identifier: identifier, importerexporter_id: importer_id)
+      if importer_run_id
+        # account for the possibility that the same record may have successfully or unsuccessfully
+        # been imported in a different importer
+        importer_id = ImporterRun.find(importer_run_id).importer_id
+        record = Entry.find_by(identifier: identifier, importerexporter_id: importer_id)
+      else
+        # TODO(alishaevn): figure out how to access the importer_run_id in the "create_file_set" method
+        # so this else can be removed
+        record = Entry.find_by(identifier: identifier)
+      end
       record ||= ::Collection.where(id: identifier).first # rubocop:disable Rails/FindBy
       if record.blank?
         available_work_types.each do |work_type|
