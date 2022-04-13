5b3db3608fad0a7e4ca59cc6300e47906824c951
braydonjustice <braydonjustice@gmail.com>
2022-04-13 12:00:10 +0900

    remove database column for parents and just use relationships look up each time.

--- app/models/bulkrax/importer_run.rb
+++ app/models/bulkrax/importer_run.rb
@@ -4,5 +4,9 @@ module Bulkrax
   class ImporterRun < ApplicationRecord
     belongs_to :importer
     has_many :statuses, as: :runnable, dependent: :destroy
+
+    def parents
+      PendingRelationship.where(bulkrax_importer_run_id: id).pluck(:parent_id).uniq
+    end
   end
 end
--- db/migrate/20220301020307_add_parents_to_bulkrax_importer_runs.rb
+++
@@ -1,5 +0,0 @@
-class AddParentsToBulkraxImporterRuns < ActiveRecord::Migration[5.1]
-  def change
-    add_column :bulkrax_importer_runs, :parents, :text, array: true, default: "{}"
-  end
-end
