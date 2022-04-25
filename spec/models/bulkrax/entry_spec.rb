ba9e334ad1b75b2396e6f12a3acadf51d369f2f5
Shana <shana.lavina.moore@gmail.com>
2022-04-25 16:51:23 -0700

    Update entry_spec.rb

--- spec/models/bulkrax/entry_spec.rb
+++ spec/models/bulkrax/entry_spec.rb
@@ -86,17 +86,17 @@ module Bulkrax
       subject { described_class.new(importerexporter: exporter) }
       let(:exporter) do
         FactoryBot.build(:bulkrax_exporter, field_mapping: {
-                            'creator' => {
-                              from: ['author'],
-                              parsed: false,
-                              split: false,
-                              if: nil,
-                              excluded: false
-                            },
-                            'first_name' => { from: ['first_name'] },
-                            'last_name' => { from: ['last_name'] },
-                            'date_uploaded' => { from: ['date_uploaded'], split: '\|', generated: true }
-                          })
+                                                              'creator' => {
+                                                                from: ['author'],
+                                                                parsed: false,
+                                                                split: false,
+                                                                if: nil,
+                                                                excluded: false
+                                                              },
+                                                              'first_name' => { from: ['first_name'] },
+                                                              'last_name' => { from: ['last_name'] },
+                                                              'date_uploaded' => { from: ['date_uploaded'], split: '\|', generated: true }
+                                                            })
       end
 
       context 'when generated_metadata is false' do
