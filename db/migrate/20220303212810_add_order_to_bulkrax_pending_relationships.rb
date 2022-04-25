46efeba037e3aa99f5a4a02b205fc770a46af9a3
Shana Moore <shana.lavina.moore@gmail.com>
2022-04-25 08:22:56 -0700

    i438 - fix erroring migrations (#487)

--- db/migrate/20220303212810_add_order_to_bulkrax_pending_relationships.rb
+++ db/migrate/20220303212810_add_order_to_bulkrax_pending_relationships.rb
@@ -1,5 +1,5 @@
 class AddOrderToBulkraxPendingRelationships < ActiveRecord::Migration[5.2]
   def change
-    add_column :bulkrax_pending_relationships, :order, :integer, default: 0
+    add_column :bulkrax_pending_relationships, :order, :integer, default: 0 unless column_exists?(:bulkrax_pending_relationships, :order)
   end
 end
