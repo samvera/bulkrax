57903eb64df70afef0dfb07a55d3c2ce3faa2a10
Braydon Justice <sephirothkod@users.noreply.github.com>
2022-05-05 01:10:14 -0700

    remove ruby 2.5 from testing as 2.5 is EOL (#507)
    * remove ruby 2.5 from testing as 2.5 is EOL. #491
    
    * spec update

--- spec/jobs/bulkrax/importer_job_spec.rb
+++ spec/jobs/bulkrax/importer_job_spec.rb
@@ -30,7 +30,7 @@ module Bulkrax
         importer_job.perform(1)
 
         expect(importer.current_run.total_work_entries).to eq(10)
-        expect(importer.current_run.total_collection_entries).to eq(426)
+        expect(importer.current_run.total_collection_entries).to eq(427)
       end
     end
 
