e246b1474e5fa242e8aeebcdb737005bf75cdf3b
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2022-05-18 17:30:05 -0700

    Fix Collection-to-Work relationship assigning (#519)
    * ensure Entry's parsed_metadata always gets persisted
    
    * do not try to add collection-work memberships as collection-collection memberships
    
    * semantic improvement - collections need to exist before works
    
    * update :processed_relationships counters by correct amount
    
    * update for #collection_parent_work_child specs, fix specs that should be using IDs instead of identifiers

--- app/models/concerns/bulkrax/import_behavior.rb
+++ app/models/concerns/bulkrax/import_behavior.rb
@@ -21,6 +21,8 @@ module Bulkrax
         status_info(e)
       else
         status_info
+      ensure
+        self.save!
       end
       return @item
     end
--- spec/jobs/bulkrax/create_relationships_job_spec.rb
+++ spec/jobs/bulkrax/create_relationships_job_spec.rb
@@ -30,80 +30,76 @@ module Bulkrax
 
     describe '#perform' do
       context 'when adding a child work to a parent collection' do
-        let(:collection_work_attrs) do
-          {
-            parent: parent_record,
-            child: child_record
-          }
+        before do
+          allow(Bulkrax::PendingRelationship).to receive(:find_each).and_return([pending_rel])
         end
 
         context 'with a Bulkrax::Entry source_identifier' do
           it 'calls #collection_parent_work_child' do
             expect(create_relationships_job).to receive(:collection_parent_work_child)
-            allow(Bulkrax::PendingRelationship).to receive(:find_each).and_return([pending_rel])
+
             create_relationships_job.perform(
               parent_identifier: parent_entry.identifier, # source_identifier
               importer_run_id: importer.current_run.id
             )
           end
 
-          it 'runs NestedCollectionPersistenceService' do
-            expect(::Hyrax::Collections::NestedCollectionPersistenceService).to receive(:persist_nested_collection_for).with(collection_work_attrs)
-            allow(Bulkrax::PendingRelationship).to receive(:find_each).and_return([pending_rel])
+          it 'calls #add_member_objects on the parent record' do
+            expect(parent_record).to receive(:add_member_objects).with([child_record.id])
+
             create_relationships_job.perform(
               parent_identifier: parent_entry.identifier,
               importer_run_id: importer.current_run.id
             )
           end
 
-          context 'importer run' do
-            it 'increments processed relationships' do
-              allow(Bulkrax::PendingRelationship).to receive(:find_each).and_return([pending_rel])
-              allow(::Hyrax::Collections::NestedCollectionPersistenceService).to receive(:persist_nested_collection_for).with(collection_work_attrs).and_return(true)
-              create_relationships_job.perform(
-                parent_identifier: parent_entry.identifier,
-                importer_run_id: importer.current_run.id
-              )
-              expect(importer.current_run.reload.processed_relationships).to eq(1)
-            end
+          it 'increments the processed relationships counter on the importer run' do
+            allow(parent_record).to receive(:add_member_objects)
+            create_relationships_job.perform(
+              parent_identifier: parent_entry.identifier,
+              importer_run_id: importer.current_run.id
+            )
+
+            expect(importer.reload.last_run.processed_relationships).to eq(1)
           end
         end
 
         context 'with an ID' do
+          let(:pending_rel) { build(:pending_relationship_collection_parent, parent_id: parent_record.id) }
+
           before do
-            allow(Entry).to receive(:find_by).with(identifier: parent_record.id).and_return(nil)
-            allow(::Collection).to receive(:where).with(id: parent_record.id).and_return([parent_record])
-            allow(::Work).to receive(:where).with(id: child_record.id).and_return([child_record])
-            allow(Bulkrax::PendingRelationship).to receive(:find_each).and_return([pending_rel])
+            importer.current_run
+            allow(create_relationships_job)
+              .to receive(:find_record)
+              .and_return([nil, parent_record], [child_entry, child_record])
           end
 
           it 'calls #collection_parent_work_child' do
             expect(create_relationships_job).to receive(:collection_parent_work_child)
 
             create_relationships_job.perform(
-              parent_identifier: parent_entry.identifier,
+              parent_identifier: parent_record.id,
               importer_run_id: importer.current_run.id
             )
           end
 
-          it 'runs NestedCollectionPersistenceService' do
-            expect(::Hyrax::Collections::NestedCollectionPersistenceService).to receive(:persist_nested_collection_for).with(collection_work_attrs)
+          it 'calls #add_member_objects on the parent record' do
+            expect(parent_record).to receive(:add_member_objects).with([child_record.id])
+
             create_relationships_job.perform(
-              parent_identifier: parent_entry.identifier,
+              parent_identifier: parent_record.id,
               importer_run_id: importer.current_run.id
             )
           end
 
-          context 'importer run' do
-            it 'increments processed relationships' do
-              allow(Bulkrax::PendingRelationship).to receive(:find_each).and_return([pending_rel])
-              allow(::Hyrax::Collections::NestedCollectionPersistenceService).to receive(:persist_nested_collection_for).with(collection_work_attrs).and_return(true)
-              create_relationships_job.perform(
-                parent_identifier: parent_entry.identifier,
-                importer_run_id: importer.current_run.id
-              )
-              expect(importer.current_run.reload.processed_relationships).to eq(1)
-            end
+          it 'increments the processed relationships counter on the importer run' do
+            allow(parent_record).to receive(:add_member_objects)
+            create_relationships_job.perform(
+              parent_identifier: parent_record.id,
+              importer_run_id: importer.current_run.id
+            )
+
+            expect(importer.reload.last_run.processed_relationships).to eq(1)
           end
         end
       end
@@ -156,18 +152,21 @@ module Bulkrax
         end
 
         context 'with an ID' do
+          let(:pending_rel_col) { build(:pending_relationship_collection_child, parent_id: parent_record.id) }
+
           before do
-            allow(Entry).to receive(:find_by).with(identifier: parent_record.id).and_return(nil)
-            allow(::Collection).to receive(:where).with(id: parent_record.id).and_return([parent_record])
-            allow(::Work).to receive(:where).with(id: child_record.id).and_return([child_record])
+            importer.current_run
             allow(Bulkrax::PendingRelationship).to receive(:find_each).and_return([pending_rel_col])
+            allow(create_relationships_job)
+              .to receive(:find_record)
+              .and_return([nil, parent_record], [child_entry, child_record])
           end
 
           it 'calls #collection_parent_collection_child' do
             expect(create_relationships_job).to receive(:collection_parent_collection_child)
 
             create_relationships_job.perform(
-              parent_identifier: parent_entry.identifier,
+              parent_identifier: parent_record.id,
               importer_run_id: importer.current_run.id
             )
           end
@@ -176,7 +175,7 @@ module Bulkrax
             expect(::Hyrax::Collections::NestedCollectionPersistenceService).to receive(:persist_nested_collection_for).with(collection_collection_attrs)
 
             create_relationships_job.perform(
-              parent_identifier: parent_entry.identifier,
+              parent_identifier: parent_record.id,
               importer_run_id: importer.current_run.id
             )
           end
@@ -186,7 +185,7 @@ module Bulkrax
               allow(Bulkrax::PendingRelationship).to receive(:find_each).and_return([pending_rel_col])
               allow(::Hyrax::Collections::NestedCollectionPersistenceService).to receive(:persist_nested_collection_for).with(collection_collection_attrs).and_return(true)
               create_relationships_job.perform(
-                parent_identifier: parent_entry.identifier,
+                parent_identifier: parent_record.id,
                 importer_run_id: importer.current_run.id
               )
               expect(importer.current_run.reload.processed_relationships).to eq(1)
@@ -238,10 +237,14 @@ module Bulkrax
         end
 
         context 'with an ID' do
+          let(:pending_rel_work) { build(:pending_relationship_collection_child, parent_id: parent_record.id) }
+
           before do
-            allow(Entry).to receive(:find_by).with(identifier: parent_record.id).and_return(nil)
-            allow(::Collection).to receive(:where).with(id: parent_record.id).and_return([parent_record])
-            allow(::Work).to receive(:where).with(id: child_record.id).and_return([child_record])
+            importer.current_run
+            allow(Bulkrax::PendingRelationship).to receive(:find_each).and_return([pending_rel_work])
+            allow(create_relationships_job)
+              .to receive(:find_record)
+              .and_return([nil, parent_record], [child_entry, child_record])
           end
 
           it 'calls #work_parent_work_child' do
@@ -249,7 +252,7 @@ module Bulkrax
             allow(Bulkrax::PendingRelationship).to receive(:find_each).and_return([pending_rel_work])
 
             create_relationships_job.perform(
-                parent_identifier: parent_entry.identifier,
+                parent_identifier: parent_record.id,
                 importer_run_id: importer.current_run.id
               )
           end
@@ -259,7 +262,7 @@ module Bulkrax
             allow(Ability).to receive(:new).with(importer.user)
             expect(Hyrax::CurationConcern.actor).to receive(:update).with(instance_of(env))
             create_relationships_job.perform(
-                parent_identifier: parent_entry.identifier,
+                parent_identifier: parent_record.id,
                 importer_run_id: importer.current_run.id
               )
           end
@@ -270,7 +273,7 @@ module Bulkrax
               allow(Ability).to receive(:new).with(importer.user)
               allow(Hyrax::CurationConcern.actor).to receive(:update).with(instance_of(env)).and_return(true)
               create_relationships_job.perform(
-                parent_identifier: parent_entry.identifier,
+                parent_identifier: parent_record.id,
                 importer_run_id: importer.current_run.id
               )
               expect(importer.current_run.reload.processed_relationships).to equal(1)
