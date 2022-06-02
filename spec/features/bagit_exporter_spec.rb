c8b3b95149744b75cf6f1b24d35b3b01bc76ddc3
Shana <shana.lavina.moore@gmail.com>
2022-06-02 07:53:53 -0700

    Update bagit_exporter_spec.rb

--- spec/features/bagit_exporter_spec.rb
+++ spec/features/bagit_exporter_spec.rb
@@ -8,10 +8,7 @@ module Bulkrax
     let(:exporter) { FactoryBot.create(:bulkrax_exporter, parser_klass: 'Bulkrax::BagitParser', export_from: 'all') }
     let(:bulkrax_exporter_run) { FactoryBot.create(:bulkrax_exporter_run, exporter: exporter) }
     before do
-      importer.import_works
-      exporter.parser.instance_variable_set(:@work_ids, importer.entries.pluck(:identifier))
-      exporter.parser.instance_variable_set(:@collection_ids, [])
-      exporter.parser.instance_variable_set(:@file_set_ids, [])
+      importer.impxort_works
       allow(exporter.parser).to receive(:current_record_ids).and_return(importer.entries.pluck(:identifier))
     end
 
