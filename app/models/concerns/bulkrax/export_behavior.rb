d68bb6b8175af2b26b576dc3fef4cc96238ce0ce
Alisha Evans <alisha@notch8.com>
2022-06-02 19:07:37 -0500

    do not prepend the file id to the file name on bagit export

--- app/models/concerns/bulkrax/export_behavior.rb
+++ app/models/concerns/bulkrax/export_behavior.rb
@@ -53,7 +53,7 @@ module Bulkrax
       fn = file_set.original_file.file_name.first
       mime = Mime::Type.lookup(file_set.original_file.mime_type)
       ext_mime = MIME::Types.of(file_set.original_file.file_name).first
-      if fn.include?(file_set.id) || importerexporter.metadata_only?
+      if fn.include?(file_set.id) || importerexporter.metadata_only? || importerexporter.parser_klass.include?('Bagit')
         filename = "#{fn}.#{mime.to_sym}"
         filename = fn if mime.to_s == ext_mime.to_s
       else
