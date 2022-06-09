9e146b665ba478b7ff2bf3ee2e01ca79772fb1e3
Benjamin Kiah Stroud <kiah@notch8.com>
2022-06-08 17:30:18 -0700

    let Rails handle db table prefixes with its magic :crystal_ball:

--- app/models/bulkrax/importer_run.rb
+++ app/models/bulkrax/importer_run.rb
@@ -4,6 +4,7 @@ module Bulkrax
   class ImporterRun < ApplicationRecord
     belongs_to :importer
     has_many :statuses, as: :runnable, dependent: :destroy
+    has_many :pending_relationships, dependent: :destroy
 
     def parents
       PendingRelationship.where(bulkrax_importer_run_id: id).pluck(:parent_id).uniq
