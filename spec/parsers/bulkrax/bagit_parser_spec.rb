0cbb10cc22f567b418f09fc7a6f9a0d9f31f8e44
Alisha Evans <alisha@notch8.com>
2022-06-20 18:15:55 -0500

    specs and lint passes!

--- spec/parsers/bulkrax/bagit_parser_spec.rb
+++ spec/parsers/bulkrax/bagit_parser_spec.rb
@@ -30,23 +30,12 @@ module Bulkrax
 
         describe 'Bag or Bags with RDF Metadata' do
           let(:entry) { FactoryBot.create(:bulkrax_rdf_entry, importerexporter: rdf_importer) }
-          let(:parser_fields) do
-            {
-              'metadata_file_name' => 'metadata.nt',
-              'metadata_format' => 'Bulkrax::RdfEntry'
-            }
-          end
 
           before do
             allow(Bulkrax::RdfEntry).to receive_message_chain(:where, :first_or_create!).and_return(entry)
-            rdf_importer.parser_fields = parser_fields
           end
 
           context 'Bag containing RDF' do
-            before do
-              rdf_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/bag"
-            end
-
             it 'creates the entry and increments the counters' do
               expect(subject).to receive(:increment_counters).once
               subject.create_works
@@ -54,44 +43,36 @@ module Bulkrax
           end
 
           context 'Bag with no metadata' do
-            before do
-              rdf_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/bag_nometadata"
-            end
-
             it 'Raises an error' do
+              rdf_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/bag_nometadata"
+
               subject.create_works
               expect(rdf_importer.last_error['error_class']).to eq('StandardError')
             end
           end
 
           context 'Invalid - not a bag' do
-            before do
-              rdf_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/spec/fixtures/bags/not_a_bag"
-            end
-
             it 'Raises an error' do
+              rdf_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/spec/fixtures/bags/not_a_bag"
+
               subject.create_works
               expect(rdf_importer.last_error['error_class']).to eq('StandardError')
             end
           end
 
           context 'Bag containing folders' do
-            before do
-              rdf_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/bag_of_folders_with_rdf"
-            end
-
             it 'creates the entry and increments the counters' do
+              rdf_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/bag_of_folders_with_rdf"
+
               expect(subject).to receive(:increment_counters).once
               subject.create_works
             end
           end
 
           context 'Folder containing bags' do
-            before do
-              rdf_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/folder_of_bags_with_rdf"
-            end
-
             it 'creates the entry and increments the counters' do
+              rdf_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/folder_of_bags_with_rdf"
+
               expect(subject).to receive(:increment_counters).once
               subject.create_works
             end
@@ -158,23 +139,12 @@ module Bulkrax
 
         describe 'Bag or Bags with CSV Metadata' do
           let(:entry) { FactoryBot.create(:bulkrax_csv_entry, importerexporter: csv_importer) }
-          let(:parser_fields) do
-            {
-              'metadata_file_name' => 'metadata.csv',
-              'metadata_format' => 'Bulkrax::CsvEntry'
-            }
-          end
 
           before do
             allow(Bulkrax::CsvEntry).to receive_message_chain(:where, :first_or_create!).and_return(entry)
-            csv_importer.parser_fields = parser_fields
           end
 
           context 'Bag containing CSV' do
-            before do
-              csv_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/bag_with_csv"
-            end
-
             it 'creates the entry and increments the counters' do
               expect(subject).to receive(:increment_counters).once
               subject.create_works
@@ -182,44 +152,36 @@ module Bulkrax
           end
 
           context 'Bag with no metadata' do
-            before do
-              csv_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/bag_nometadata"
-            end
-
             it 'Raises an error' do
+              csv_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/bag_nometadata"
+
               subject.create_works
               expect(csv_importer.last_error['error_class']).to eq('StandardError')
             end
           end
 
           context 'Invalid - not a bag' do
-            before do
-              csv_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/spec/fixtures/bags/not_a_bag"
-            end
-
             it 'Raises an error' do
+              csv_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/spec/fixtures/bags/not_a_bag"
+
               subject.create_works
               expect(csv_importer.last_error['error_class']).to eq('StandardError')
             end
           end
 
           context 'Bag containing folders' do
-            before do
-              csv_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/bag_of_folders_with_csv"
-            end
-
             it 'creates the entry and increments the counters' do
+              csv_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/bag_of_folders_with_csv"
+
               expect(subject).to receive(:increment_counters).once
               subject.create_works
             end
           end
 
           context 'Folder containing bags' do
-            before do
-              csv_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/folder_of_bags_with_csv"
-            end
-
             it 'creates the entry and increments the counters' do
+              csv_importer.parser_fields['import_file_path'] = "./spec/fixtures/bags/folder_of_bags_with_csv"
+
               expect(subject).to receive(:increment_counters).once
               subject.create_works
             end
