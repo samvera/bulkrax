272013a69b6ea629a7347c64c5640182c38c0df2
Alisha Evans <alisha@notch8.com>
2022-07-06 17:50:05 -0500

    lint fix

--- app/parsers/bulkrax/csv_parser.rb
+++ app/parsers/bulkrax/csv_parser.rb
@@ -319,22 +319,26 @@ module Bulkrax
             csv << entry.parsed_metadata
             next if importerexporter.metadata_only? || entry.type == 'Bulkrax::CsvCollectionEntry'
 
-            record = ActiveFedora::Base.find(entry.identifier)
-            file_sets = record.file_set? ? Array.wrap(record) : record.file_sets
-            file_sets << record.thumbnail if exporter.include_thumbnails && record.thumbnail.present? && record.work?
-            file_sets.each do |fs|
-              path = File.join(exporter_export_path, folder_count.to_s, 'files')
-              FileUtils.mkdir_p(path) unless File.exist? path
-              file = filename(fs)
-              io = open(fs.original_file.uri)
-              next if file.blank?
-
-              File.open(File.join(path, file), 'wb') do |f|
-                f.write(io.read)
-                f.close
-              end
-            end
+            store_files(entry.identifier, folder_count.to_s)
           end
+        end
+      end
+    end
+
+    def store_files(identifier, folder_count)
+      record = ActiveFedora::Base.find(identifier)
+      file_sets = record.file_set? ? Array.wrap(record) : record.file_sets
+      file_sets << record.thumbnail if exporter.include_thumbnails && record.thumbnail.present? && record.work?
+      file_sets.each do |fs|
+        path = File.join(exporter_export_path, folder_count, 'files')
+        FileUtils.mkdir_p(path) unless File.exist? path
+        file = filename(fs)
+        io = open(fs.original_file.uri)
+        next if file.blank?
+
+        File.open(File.join(path, file), 'wb') do |f|
+          f.write(io.read)
+          f.close
         end
       end
     end
