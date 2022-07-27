90f6cd67072fe7f19b077dbe07bcf66fc424b312
braydonjustice <braydonjustice@gmail.com>
2022-07-27 11:58:54 +0900

    helps specs

--- spec/test_app/app/models/site.rb
+++ spec/test_app/app/models/site.rb
@@ -2,13 +2,17 @@
 
 class Site < ApplicationRecord
   class << self
-    delegate :account, to: :instance
 
     def instance
+      Site.create
       # return NilSite.instance if Account.global_tenant?
       # first_or_create do |site|
       #   site.available_works = Hyrax.config.registered_curation_concern_types
       # end
     end
   end
+
+    def account
+      "account"
+    end
 end
