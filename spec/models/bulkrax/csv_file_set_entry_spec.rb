acbdc78f3af12e5f4e2effe3627e6ee3576883e3
Alisha Evans <alisha@notch8.com>
2022-08-25 09:36:13 -0500

    add specs for #file_reference

--- spec/models/bulkrax/csv_file_set_entry_spec.rb
+++ spec/models/bulkrax/csv_file_set_entry_spec.rb
@@ -6,6 +6,28 @@ module Bulkrax
   RSpec.describe CsvFileSetEntry, type: :model do
     subject(:entry) { described_class.new }
 
+    describe '#file_reference' do
+      context 'when parsed_metadata includes the "file" property' do
+        before do
+          entry.parsed_metadata = { 'file' => ['test.png'] }
+        end
+
+        it 'returns the correct key' do
+          expect(entry.file_reference).to eq('file')
+        end
+      end
+
+      context 'when parsed_metadata includes the "remote_files" property' do
+        before do
+          entry.parsed_metadata = { 'remote_files' => ['test.png'] }
+        end
+
+        it 'returns the correct key' do
+          expect(entry.file_reference).to eq('remote_files')
+        end
+      end
+    end
+
     describe '#validate_presence_of_filename!' do
       context 'when filename is missing' do
         before do
