d45a8846002af2c89dc722acc0c06401b0d16ba8
Shana Moore <shana.lavina.moore@gmail.com>
2022-11-02 15:28:30 -0700

    Merge default field mapping in with field mapping (#664)
    * Merge default field mapping in with field mapping to prevent multiple error
    
    A bug was found with the following scenario:
    
    A client only had the necessary field mappings set (bulkrax_identifier, children, and parents). Because of this, [#mapping](https://github.com/samvera-labs/bulkrax/blob/main/app/models/bulkrax/importer.rb#L60-L72) would only retrun the field mapping. This caused an issue because [matcher](https://github.com/samvera-labs/bulkrax/blob/e88d0ea349afda5b3068836aa36ef86c93d4c62d/app/models/concerns/bulkrax/has_matchers.rb#L32) would return nil for all other values.
    
    Because [it](https://github.com/samvera-labs/bulkrax/blob/e88d0ea349afda5b3068836aa36ef86c93d4c62d/app/models/concerns/bulkrax/has_matchers.rb#L44) was nil at the time of assigning the value, if the property was multiple it would never process the split method to make [Subject A ; Subject B] into [Subject A, Subject B].
    
    This was also tested using an importer with headers that didn't exist on any of the work types. Although it can be seen in the raw_metdata, parsed_metadata properly filters it out because #field_supported? returns false.
    
    * Update app/models/bulkrax/importer.rb
    
    Co-authored-by: Alisha Evans <alisha@notch8.com>
    
    * update spec setup
    
    * fix rubocop
    
    * remove whitespace
    
    Co-authored-by: Alisha Evans <alisha@notch8.com>

--- app/models/bulkrax/importer.rb
+++ app/models/bulkrax/importer.rb
@@ -58,17 +58,26 @@ module Bulkrax
 
     # If field_mapping is empty, setup a default based on the export_properties
     def mapping
+      # rubocop:disable Style/IfUnlessModifier
       @mapping ||= if self.field_mapping.blank? || self.field_mapping == [{}]
                      if parser.import_fields.present? || self.field_mapping == [{}]
-                       ActiveSupport::HashWithIndifferentAccess.new(
-                         parser.import_fields.reject(&:nil?).map do |m|
-                           Bulkrax.default_field_mapping.call(m)
-                         end.inject(:merge)
-                       )
+                       default_field_mapping
                      end
                    else
-                     self.field_mapping
+                     default_field_mapping.merge(self.field_mapping)
                    end
+
+      # rubocop:enable Style/IfUnlessModifier
+    end
+
+    def default_field_mapping
+      return self.field_mapping if parser.import_fields.nil?
+
+      ActiveSupport::HashWithIndifferentAccess.new(
+        parser.import_fields.reject(&:nil?).map do |m|
+          Bulkrax.default_field_mapping.call(m)
+        end.inject(:merge)
+      )
     end
 
     def parser_fields
