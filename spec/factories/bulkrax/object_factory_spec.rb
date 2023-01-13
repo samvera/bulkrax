1c5d0ef9b7694ad15c655aa141d65f9a63ef4c03
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-01-13 11:08:17 -0500

    Scour blank strings from the creation attributes
    There are three changes in this commit:
    
    1. Exposing a class attribute
    2. Scouring attribute hash
    3. Extracting a method
    
    **Exposing a class attribute**
    
    This was added to allow for easier testing of a private method.  It also
    "exposes" a hidden assumption in a private method; turning that
    assumption into a telegraphed potential configuration point.
    
    **Scouring attribute hash**
    
    This resolves the reported problem by @eporter23:
    
    > When testing an import on 9/16/22, the work row failed to validate,
    > returning an error regarding "invalid entry for administrative_unit".
    >
    > The CSV used had the header for that field, but no values entered. Our
    > preprocessors output field headers for mapped fields, but sometimes
    > there are no values entered for a given header.
    >
    > -- https://github.com/emory-libraries/dlp-curate/issues/1973
    
    The likely most vexing problem being an array that has only empty
    values.  This is likely something that should be remedied upstream in
    the data modeling for Hyrax models; but alas we're not there yet.
    
    **Extracting a method**
    
    This is a happy little refactor to create symmetry between the other
    create/update methods for collections and file sets.  In doing this, we
    provide a mechanism for folks to more readily over-ride methods at a
    lower level.
    
    Related to:
    
    - https://github.com/emory-libraries/dlp-curate/issues/1973
    - https://github.com/emory-libraries/dlp-curate/pull/1981

---
+++ spec/factories/bulkrax/object_factory_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Bulkrax::ObjectFactory do
+  # Why aren't there more tests?  In part because so much of the ObjectFactory require that we boot
+  # up Fedora and SOLR; something that remains non-desirous due to speed.
+  describe "#transform_attributes" do
+    it "empties arrays that only have empty values" do
+      attributes = { empty_array: ["", ""], empty_string: "", filled_array: ["A", "B"], filled_string: "A" }
+      factory = described_class.new(attributes: attributes, source_identifier_value: 123, work_identifier: "filled_string")
+      factory.base_permitted_attributes = %i[empty_array empty_string filled_array filled_string]
+      expect(factory.send(:transform_attributes))
+        .to eq({ empty_array: [], empty_string: nil, filled_array: ["A", "B"], filled_string: "A" }.stringify_keys)
+    end
+  end
+end
