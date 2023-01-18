samvera/bulkrax

commit 304e2a8733f57ef1ffce9f0f7ff744376f0f402f

Refactoring to remove conditional (#663)
Refactoring Bulkrax::ApplicationParser conditional

Prior to this commit, we had an if statement that would check if the
given parser was a `Bulkrax::CsvParser`, if so it would go down one
logic pathway.  If not, it would call a different pathway.  Each pathway
required knowledge of the implementation details of the parser.  The
`Bulkrax::CsvParser` required knowledge of a non-public API
setup (e.g. a method not defined on `Bulkrax::ApplicationParser`).

With this commit, we're favoring a single entry point for all parsers.
Moving the logic down to the parser and allowing for polymorphism of the
`Bulkrax::CsvParser` to provide it's different implementation.

One additional change is calling save on the importer before we create
the objects.  This was something not called in the `Bulkrax::CsvParser`
and based on the comment could have created a problem.

app/parsers/bulkrax/application_parser.rb
@@ -142,6 +142,21 @@ module Bulkrax
       @visibility ||= self.parser_fields['visibility'] || 'open'
     end
 
+    # @api public
+    #
+    # @param types [Array<Symbol>] the types of objects that we'll create.
+    #
+    # @see Bulkrax::Importer::DEFAULT_OBJECT_TYPES
+    # @see #create_collections
+    # @see #create_works
+    # @see #create_file_sets
+    # @see #create_relationships
+    def create_objects(types = [])
+      types.each do |object_type|
+        parser.send("create_#{object_type.pluralize}")
+      end
+    end
+
     # @abstract Subclass and override {#create_collections} to implement behavior for the parser.
     def create_collections
       raise NotImplementedError, 'must be defined' if importer?

app/models/bulkrax/importer.rb
@@ -143,17 +143,13 @@ module Bulkrax
       import_objects(['relationship'])
     end
 
+    DEFAULT_OBJECT_TYPES = %w[collection work file_set relationship].freeze
+
     def import_objects(types_array = nil)
       self.only_updates ||= false
-      types = types_array || %w[collection work file_set relationship]
-      if parser.class == Bulkrax::CsvParser
-        parser.create_objects(types)
-      else
-        types.each do |object_type|
-          self.save if self.new_record? # Object needs to be saved for statuses
-          parser.send("create_#{object_type.pluralize}")
-        end
-      end
+      self.save if self.new_record? # Object needs to be saved for statuses
+      types = types_array || DEFAULT_OBJECT_TYPES
+      parser.create_objects(types)
     rescue StandardError => e
       status_info(e)
     end

commit 9b2b222d69895be95c1b241cbe91327e19513809

Fixing bug introduced in #663
Prior to this commit, I had refactored (in PR #663) logic to remove
conditional logic.  However, I had introduced a bug, namely using
`parser` and the receiver of `send`.  For the
`Bulkrax::ApplicationParser` there is no parser method.  This resulted
in a notable bug.

With this commit, I'm removing `parser` as the receiver of `send` and
instead favoring the implicit `self` as the receiver of `send`.

app/parsers/bulkrax/application_parser.rb
@@ -153,7 +153,7 @@ module Bulkrax
     # @see #create_relationships
     def create_objects(types = [])
       types.each do |object_type|
-        parser.send("create_#{object_type.pluralize}")
+        send("create_#{object_type.pluralize}")
       end
     end
 

spec/parsers/bulkrax/application_parser_spec.rb
@@ -14,6 +14,19 @@ module Bulkrax
     let(:site) { instance_double(Site, id: 1, account_id: 1) }
     let(:account) { instance_double(Account, id: 1, name: 'bulkrax') }
 
+    describe '#create_objects' do
+      subject(:application_parser) { described_class.new(importer) }
+
+      it 'sends the create_* methods based on given types' do
+        expect(application_parser).to receive(:create_works)
+        expect(application_parser).to receive(:create_collections)
+        expect(application_parser).to receive(:create_file_sets)
+        expect(application_parser).to receive(:create_relationships)
+
+        application_parser.create_objects(%w[collection work file_set relationship])
+      end
+    end
+
     describe '#get_field_mapping_hash_for' do
       context 'with `[{}]` as the field mapping' do
         subject(:application_parser) { described_class.new(importer) }