a3dc23eb4086192547f31a8bbeca81b344a6b7aa
Ben Pennell <ben.pennell@gmail.com>
2023-01-18 18:03:15 -0500

    Convert source_identifier from relationship to single value (#712)
    * Convert source_identifier from relationship to single value
    
    * Rubocop

--- app/models/bulkrax/csv_entry.rb
+++ app/models/bulkrax/csv_entry.rb
@@ -107,7 +107,9 @@ module Bulkrax
     # Metadata required by Bulkrax for round-tripping
     def build_system_metadata
       self.parsed_metadata['id'] = hyrax_record.id
-      self.parsed_metadata[source_identifier] = hyrax_record.send(work_identifier)
+      source_id = hyrax_record.send(work_identifier)
+      source_id = source_id.to_a.first if source_id.is_a?(ActiveTriples::Relation)
+      self.parsed_metadata[source_identifier] = source_id
       self.parsed_metadata[key_for_export('model')] = hyrax_record.has_model.first
     end
 
