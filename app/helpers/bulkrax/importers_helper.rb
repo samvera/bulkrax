04d1c322923c812cd061559de1fe199d8c5170df
Braydon Justice <sephirothkod@users.noreply.github.com>
2023-02-13 18:14:22 -0800

    only uses admin sets if Hyrax is present (#739)
    only uses admin sets in importer creation if Bulkrax is installed in a Hyrax instance.

--- app/helpers/bulkrax/importers_helper.rb
+++ app/helpers/bulkrax/importers_helper.rb
@@ -2,9 +2,9 @@
 
 module Bulkrax
   module ImportersHelper
-    # borrowd from batch-importer https://github.com/samvera-labs/hyrax-batch_ingest/blob/main/app/controllers/hyrax/batch_ingest/batches_controller.rb
+    # borrowed from batch-importer https://github.com/samvera-labs/hyrax-batch_ingest/blob/main/app/controllers/hyrax/batch_ingest/batches_controller.rb
     def available_admin_sets
-      # Restrict available_admin_sets to only those current user can desposit to.
+      # Restrict available_admin_sets to only those current user can deposit to.
       @available_admin_sets ||= Hyrax::Collections::PermissionsService.source_ids_for_deposit(ability: current_ability, source_type: 'admin_set').map do |admin_set_id|
         [AdminSet.find(admin_set_id).title.first, admin_set_id]
       end
