f25a78797bfd983821c17ca93e0c68a36ecc3ecc
Rob Kaufman <rob@notch8.com>
2023-02-14 11:50:11 -0800

    replace multiple times through the pending realtionship loop (#737)
    * replace multiple times through the pending realtionship loop with a single run that never adds all the objects to mememory
    
    * Adding specs and error handling to CreateRelationshipsJob
    
    * Moving inner loop logic to method
    
    This helps with error tracking
    
    * Appeasing the coppers
    
    * Adding permission checks to importer
    
    * Adding Bulkrax.fallback_user_for_importer_exporter_processing
    
    This helps in cases where we run the command from CLI.  We do some
    sniffing to pick a suitable user, and if that doesn't work, we fail.
    
    * fix lint error
    
    ---------
    
    Co-authored-by: Jeremy Friesen <jeremy.n.friesen@gmail.com>
    Co-authored-by: Shana <shana.lavina.moore@gmail.com>

--- app/models/bulkrax/pending_relationship.rb
+++ app/models/bulkrax/pending_relationship.rb
@@ -3,5 +3,9 @@
 module Bulkrax
   class PendingRelationship < ApplicationRecord
     belongs_to :importer_run
+
+    # Ideally we wouldn't have a column named "order", as it is a reserved SQL term.  However, if we
+    # quote the column, all is well...for the application.
+    scope :ordered, -> { order("#{quoted_table_name}.#{connection.quote_column_name('order')}") }
   end
 end
