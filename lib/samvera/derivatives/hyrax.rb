e33e454d21bcc35ee136cc33fb05a8d82db86bab
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-03-20 15:37:36 -0400

    Adding Samvera::Derivatives::Hyrax consideration
    With this commit, I begin demonstrating how the original configuration
    can be leveraged to do the derivative work.  This still assumes a
    configuration for derivatives; something that I continue to punt on
    until I have more concrete code that I can further build from.
    
    Related to:
    
    - https://github.com/samvera-labs/bulkrax/issues/760
    - https://github.com/scientist-softserv/utk-hyku/issues/343

---
+++ lib/samvera/derivatives/hyrax.rb
@@ -0,0 +1,129 @@
+# frozen_string_literal: true
+
+module Samvera
+  module Derivatives
+    # The default behavior of {Hyrax::FileSetDerivativeService} is to create a derivative and then
+    # apply it to the FileSet.  This module wraps that behavior such that we can leverage the
+    # {Samvera::Derivatives} module and interfaces to handle cases where some of the derivatives
+    # already exist.
+    module Hyrax
+      # @note This conforms to the {Hyrax::DerivativeService} interface.  The intention of this
+      #       class is to be the sole registered {Hyrax::DerivativeService.services}
+      class ServiceShim
+        # @param file_set [FileSet]
+        # @param candidate_derivative_types [Array<Symbol>] the possible types of derivatives that
+        #        we could create for this file_set.
+        # @param config [#registry_for]
+        #
+        # @todo We will want some kind of lambda to determine the candidate_derivative_types for this
+        #       file_set.
+        def initialize(file_set, candidate_derivative_types: [], config: Samvera::Derivatives.config)
+          @file_set = file_set
+          @config = config
+          @derivatives = candidate_derivative_types.map { |type| config.registry_for(type: type) }
+        end
+
+        attr_reader :file_set
+
+        # @return [Array<Samvera::Derivatives::Configuration::RegisteredType>]
+        attr_reader :derivatives
+        attr_reader :config
+
+        def valid?
+          # We have a file set, which also means a parent work.  I believe we always want this to be
+          # valid, because we want to leverage the locator/applicator behavior instead of the
+          # implicit work.
+          true
+        end
+
+        def cleanup_derivatives; end
+
+        # We have two vectors of consideration for derivative generation:
+        #
+        # - The desired derivatives for a file_set's parent work (e.g. the candidate derivatives)
+        # - The available derivatives for a file_set's mime type
+        def create_derivatives(file_path)
+          derivatives.each do |derivative|
+            Samvera::Derivatives.locate_and_apply_derivative_for(
+              file_set: file_set,
+              file_path: file_path,
+              derivative: derivative
+            )
+          end
+        end
+
+        def derivative_url(_destination_name)
+          ""
+        end
+      end
+
+      class FileApplicatorStrategy < Samvera::Derivatives::FileApplicator::Strategy
+        # With this set to true, we're telling the applicator to use the from_location
+        # (e.g. {Samvera::Derivatives::Hyrax::FileSetDerivativeServiceWrapper}) to apply the
+        # derivatives.
+        self.delegate_apply_to_given_from_location = true
+      end
+
+      class FileLocatorStrategy < Samvera::Derivatives::FileLocator::Strategy
+        # Implements {Samvera::Derivatives::FileLocator::Strategy} interface.
+        #
+        # @see Samvera::Derivatives::FileLocator::Strategy
+        #
+        # @return [Samvera::Derivatives::Hyrax::FileSetDerivativeServiceWrapper]
+        def self.locate(file_set:, file_path:, **)
+          file_set.samvera_derivatives_default_from_location_wrapper(file_path: file_path)
+        end
+      end
+
+      class FileSetDerivativeServiceWrapper
+        class_attribute :wrapped_derivative_service_class, default: ::Hyrax::FileSetDerivativesService
+
+        # @param file_set [FileSet]
+        # @param file_path [String]
+        def initialize(file_set:, file_path:)
+          @file_set = file_set
+          @file_path = file_path
+          @wrapped_derivative_service = wrapped_derivative_service_class.new(file_set)
+        end
+        attr_reader :file_set, :wrapped_derivative_service, :file_path
+
+        # @see Samvera::Derivatives::FileLocator.call
+        def present?
+          true
+        end
+
+        # @see Samvera::Derivatives::FileApplicator::Strategy
+        def apply!(*)
+          # Why the short-circuit? By the nature of the underlying
+          # ::Hyrax::FileSetDerivativeService, we generate multiple derivatives in one pass.  But
+          # with the implementation of Samvera::Derivatives, we declare the derivatives and then
+          # iterate on locating and applying them.  With this short-circuit, we will only apply the
+          # derivatives once.
+          return true if defined?(@already_applied)
+
+          return false unless wrapped_derivative_service.valid?
+
+          wrapped_derivative_service.create_derivatives(file_path)
+          @already_applied = true
+        end
+      end
+
+      ##
+      # The purpose of this module is to preserve the existing Hyrax derivative behavior while also
+      # allowing for the two-step tango of locator and applicator.
+      #
+      # @see Samvera::Derivatives.locate_and_apply_derivative_for
+      module FileSetDecorator
+        # @return [Samvera::Derivatives::Hyrax::FileSetDerivativeServiceWrapper]
+        def samvera_derivatives_default_from_location_wrapper(file_path:)
+          @samvera_derivatives_default_from_location_wrapper ||=
+            Samvera::Derivatives::Hyrax::FileSetDerivativeServiceWrapper.new(file_set: self, file_path: file_path)
+        end
+      end
+    end
+  end
+end
+
+# TODO: We are likely going to want that.
+# Hyrax::DerivativeService.services = [Samvera::Derivatives::Hyrax::ServiceShim]
+FileSet.prepend(Samvera::Derivatives::Hyrax::FileSetDecorator)
---
+++ spec/samvera/derivatives/hyrax_spec.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+require 'rails_helper'
+require 'samvera/derivatives'
+require 'samvera/derivatives/hyrax'
+
+RSpec.describe Samvera::Derivatives::Hyrax::ServiceShim do
+  let(:config) { Samvera::Derivatives::Configuration.new }
+  let(:file_set) { FileSet.new }
+  let(:other_locator) { double('Locator', locate: nil) }
+  let(:other_applicator) { double('Applicator', apply!: nil) }
+  before do
+    config.register(
+      type: :thumbnail,
+      locators: [other_locator, Samvera::Derivatives::Hyrax::FileLocatorStrategy],
+      applicators: [other_applicator, Samvera::Derivatives::Hyrax::FileApplicatorStrategy]
+    ) { |_file_set| true }
+    config.register(
+      type: :jpg,
+      locators: [other_locator, Samvera::Derivatives::Hyrax::FileLocatorStrategy],
+      applicators: [other_applicator, Samvera::Derivatives::Hyrax::FileApplicatorStrategy]
+    ) { |_file_set| true }
+  end
+
+  let(:shim) { described_class.new(file_set, candidate_derivative_types: [:thumnail, :jpg], config: config) }
+
+  subject { shim }
+
+  it { is_expected.to be_valid }
+
+  describe '#create_derivatives' do
+    let(:file_path) { __FILE__ }
+    subject { shim.create_derivatives(file_path) }
+
+    context 'when other locator and applicator are not applicable' do
+      it 'locates and applies the derivatives using the underlying service' do
+        expect_any_instance_of(Samvera::Derivatives::Hyrax::FileApplicatorStrategy).to receive(:apply!).and_call_original
+        expect_any_instance_of(Samvera::Derivatives::Hyrax::FileSetDerivativeServiceWrapper).to receive(:apply!).and_call_original
+        expect_any_instance_of(::Hyrax::FileSetDerivativesService).to receive(:valid?).and_return(true)
+        expect_any_instance_of(::Hyrax::FileSetDerivativesService).to receive(:create_derivatives).with(file_path)
+        expect(other_applicator).to receive(:apply!)
+
+        subject
+      end
+    end
+
+    context 'when the other locator finds the derivative' do
+      let(:other_locator) { double(locate: file_path) }
+
+      it 'uses that located derivative and applies it' do
+        # Yes, we will attempt to apply the derivative...
+        expect_any_instance_of(Samvera::Derivatives::Hyrax::FileApplicatorStrategy).to receive(:apply!).and_call_original
+        # ...however, we won't be leveraging the wrapper's derivative work; meaning no default behavior.
+        expect_any_instance_of(Samvera::Derivatives::Hyrax::FileSetDerivativeServiceWrapper).not_to receive(:apply!)
+        expect_any_instance_of(::Hyrax::FileSetDerivativesService).not_to receive(:valid?)
+        expect_any_instance_of(::Hyrax::FileSetDerivativesService).not_to receive(:create_derivatives)
+        expect(other_applicator).to receive(:apply!)
+
+        subject
+      end
+    end
+  end
+end
