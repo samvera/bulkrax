817e4bb826260c2b402f730e7696dd90027f0c0c
Kirk Wang <k3wang@gmail.com>
2023-07-26 09:00:41 -0700

    🐛 Modify navtabs.js (#843)
    This commit will wrap a conditional around navtabs.js to avoid erroring
    on Hyku applications.

--- app/assets/javascripts/bulkrax/navtabs.js
+++
@@ -1,7 +0,0 @@
-// enables the tabs in the importers/exporters pages.
-$(document).ready(function() {
-  $('.nav-tabs a').click(function (e) {
-    e.preventDefault();
-    $(this).tab('show');
-  });
-});
