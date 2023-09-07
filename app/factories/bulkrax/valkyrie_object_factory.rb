f6fb201998d71fa1d5a7610d7855c29fcae87e67
Shana Moore <shana@scientist.com>
2023-09-07 15:00:56 -0700

    :gear: Switch find methods with custom queries for Valkyrie

--- app/factories/bulkrax/valkyrie_object_factory.rb
+++ app/factories/bulkrax/valkyrie_object_factory.rb
@@ -32,7 +32,7 @@ module Bulkrax
       # Query can return partial matches (something6 matches both something6 and something68)
       # so we need to weed out any that are not the correct full match. But other items might be
       # in the multivalued field, so we have to go through them one at a time.
-      match = Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: source_identifier_value)
+      match = Hyrax.query_service.custom_queries.find_by_bulkrax_identifier(identifier: source_identifier_value)
 
       return match if match
     rescue => err
@@ -60,9 +60,8 @@ module Bulkrax
           # "work_resource.add_to_parent" => {parent_id: @related_parents_parsed_mapping, user: @user},
           "work_resource.add_bulkrax_files" => {files: get_s3_files(remote_files: attributes["remote_files"]), user: @user},
           "change_set.set_user_as_depositor" => {user: @user},
-          "work_resource.change_depositor" => {user: @user}
-          # TODO: uncomment when we upgrade Hyrax 4.x
-          # 'work_resource.save_acl' => { permissions_params: [attrs.try('visibility') || 'open'].compact }
+          "work_resource.change_depositor" => {user: @user},
+          'work_resource.save_acl' => { permissions_params: [attrs.try('visibility') || 'open'].compact }
         )
         .call(cx)
 
