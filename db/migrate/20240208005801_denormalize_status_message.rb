8ce15d9604ace1a68236016dbbeb3ae0373e7de4
Rob Kaufman <rob@notch8.com>
2024-02-08 06:32:06 -0800

    Denormalize Status Message to that Entry Look Up Can Be Fast (#913)
    * Thats it, Ive had enough. we need status_message as a column on entry. Today is the day
    
    * call the cops
    
    * call the cops
    
    * Add guard condition for column existence
    
    ---------
    
    Co-authored-by: Jeremy Friesen <jeremy.n.friesen@gmail.com>

---
+++ db/migrate/20240208005801_denormalize_status_message.rb
@@ -0,0 +1,7 @@
+class DenormalizeStatusMessage < ActiveRecord::Migration[5.2]
+  def change
+    add_column :bulkrax_entries, :status_message, :string, default: 'Pending' unless column_exists?(:bulkrax_entries, :status_message)
+    add_column :bulkrax_importers, :status_message, :string, default: 'Pending' unless column_exists?(:bulkrax_importers, :status_message)
+    add_column :bulkrax_exporters, :status_message, :string, default: 'Pending' unless column_exists?(:bulkrax_exporters, :status_message)
+  end
+end
