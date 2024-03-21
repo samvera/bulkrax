04b256b5ac631acae2a093fe0345764b15ee8961
Kirk Wang <kirk.wang@scientist.com>
2024-03-20 21:22:04 -0700

    🐛 Changing guard to #respond_to?(:where)
    A spec was failing with the previous way we were checking.

--- app/factories/bulkrax/object_factory.rb
+++ app/factories/bulkrax/object_factory.rb
@@ -77,7 +77,7 @@ module Bulkrax
     #
     # @see # {Wings::CustomQueries::FindBySourceIdentifier#find_by_model_and_property_value}
     def self.search_by_property(value:, klass:, field: nil, search_field: nil, name_field: nil, verify_property: false)
-      return nil if klass < Valkyrie::Resource
+      return nil unless klass.respond_to?(:where)
       # We're not going to try to match nil nor "".
       return if value.blank?
       return if verify_property && !klass.properties.keys.include?(search_field)
