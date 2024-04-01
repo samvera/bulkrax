c726754be86fd2d4ecdedf6d6d954ed03ce737be
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-04-01 11:25:14 -0400

    ♻️ Favor object factory over hard-coded

--- app/helpers/bulkrax/importers_helper.rb
+++ app/helpers/bulkrax/importers_helper.rb
@@ -6,7 +6,7 @@ module Bulkrax
     def available_admin_sets
       # Restrict available_admin_sets to only those current user can deposit to.
       @available_admin_sets ||= Hyrax::Collections::PermissionsService.source_ids_for_deposit(ability: current_ability, source_type: 'admin_set').map do |admin_set_id|
-        [Hyrax.metadata_adapter.query_service.find_by(id: admin_set_id)&.title&.first || admin_set_id, admin_set_id]
+        [Bulkrax.object_factory.find_or_nil(admin_set_id)&.title&.first  || admin_set_id, admin_set_id]
       end
     end
   end
