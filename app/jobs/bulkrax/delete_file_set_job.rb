360a44952bb5787fb38612804f9d1f629ced0747
Rob Kaufman <rob@notch8.com>
2024-06-21 12:48:06 -0700

    remove file sets from their parent when we delete them

--- app/jobs/bulkrax/delete_file_set_job.rb
+++ app/jobs/bulkrax/delete_file_set_job.rb
@@ -2,4 +2,18 @@
 
 module Bulkrax
   class DeleteFileSetJob < DeleteJob; end
+
+  def perform(entry, importer_run)
+    file_set = entry.factory.find
+    if file_set
+      parent = file_set.parent
+      om = parent.ordered_members.to_a
+      om.delete(file_set)
+      parent.ordered_members = om
+      parent.save
+    end
+
+    super
+  end
+
 end
