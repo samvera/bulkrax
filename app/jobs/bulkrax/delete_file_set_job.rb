0d63a2e5fc11e0673cafa5d013f7023a24e76ca7
Rob Kaufman <rob@notch8.com>
2024-06-21 14:24:17 -0700

    fix syntax

--- app/jobs/bulkrax/delete_file_set_job.rb
+++ app/jobs/bulkrax/delete_file_set_job.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
 module Bulkrax
-  class DeleteFileSetJob < DeleteJob; end
-
-  def perform(entry, importer_run)
-    file_set = entry.factory.find
-    if file_set
-      parent = file_set.parent
-      if parent&.respond_to?(:ordered_members)
-        om = parent.ordered_members.to_a
-        om.delete(file_set)
-        parent.ordered_members = om
-      elsif parent.respond_to?(:member_ids)
-        parent.member_ids.delete(file_set.id)
-        Hyrax.persister.save(resource: parent)
+  class DeleteFileSetJob < DeleteJob
+    def perform(entry, importer_run)
+      file_set = entry.factory.find
+      if file_set
+        parent = file_set.parent
+        if parent&.respond_to?(:ordered_members)
+          om = parent.ordered_members.to_a
+          om.delete(file_set)
+          parent.ordered_members = om
+        elsif parent.respond_to?(:member_ids)
+          parent.member_ids.delete(file_set.id)
+          Hyrax.persister.save(resource: parent)
+        end
+        parent.save
       end
-      parent.save
-    end
 
-    super
+      super
+    end
   end
 end
