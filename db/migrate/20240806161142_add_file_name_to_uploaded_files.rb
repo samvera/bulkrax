c0daa984488c11616d3803c6b85cff1669280315
Shana Moore <shana@scientist.com>
2024-08-13 16:24:49 -0700

    :broom: fix for failing migration
    error: NoMethodError: undefined method `column_exists' for #<AddFileNameToUploadedFiles:0x000055d8c48ea0b0 @name="AddFileNameToUploadedFiles", @version=20240806161142, @connection=nil>

--- db/migrate/20240806161142_add_file_name_to_uploaded_files.rb
+++ db/migrate/20240806161142_add_file_name_to_uploaded_files.rb
@@ -1,5 +1,5 @@
 class AddFileNameToUploadedFiles < ActiveRecord::Migration[6.1]
   def change
-    add_column :uploaded_files, :filename, :string unless column_exists(:uploaded_files, :filename)
+    add_column :uploaded_files, :filename, :string unless column_exists?(:uploaded_files, :filename)
   end
 end
