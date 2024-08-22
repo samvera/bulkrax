ec8be99e3678d2760381ca5b4e63aee2f288bde7
Rob Kaufman <rob@notch8.com>
2024-08-21 18:04:23 -0700

    fix bad migration version

--- db/migrate/20240806161142_add_file_name_to_uploaded_files.rb
+++ db/migrate/20240806161142_add_file_name_to_uploaded_files.rb
@@ -1,4 +1,4 @@
-class AddFileNameToUploadedFiles < ActiveRecord::Migration[6.1]
+class AddFileNameToUploadedFiles < ActiveRecord::Migration[5.2]
   def change
     add_column :uploaded_files, :filename, :string unless column_exists?(:uploaded_files, :filename)
   end
