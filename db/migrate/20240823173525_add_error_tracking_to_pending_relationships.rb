499fb95108740784e52f7ba53027290e74995a65
Rob Kaufman <rob@notch8.com>
2024-08-23 11:56:08 -0700

    fix new migration again

--- db/migrate/20240823173525_add_error_tracking_to_pending_relationships.rb
+++ db/migrate/20240823173525_add_error_tracking_to_pending_relationships.rb
@@ -1,4 +1,4 @@
-class AddErrorTrackingToPendingRelationships < ActiveRecord::Migration[6.1]
+class AddErrorTrackingToPendingRelationships < ActiveRecord::Migration[5.1]
   def change
     add_column :bulkrax_pending_relationships, :status_message, :string, default: 'Pending' unless column_exists?(:bulkrax_pending_relationships, :status_message)
   end
