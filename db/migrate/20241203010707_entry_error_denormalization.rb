a94e796883ced0bcd9b5b0478022d128ae073627
Rob Kaufman <rob@notch8.com>
2024-12-02 21:52:34 -0800

    migration version fix

--- db/migrate/20241203010707_entry_error_denormalization.rb
+++ db/migrate/20241203010707_entry_error_denormalization.rb
@@ -1,4 +1,4 @@
-class EntryErrorDenormalization < ActiveRecord::Migration[6.1]
+class EntryErrorDenormalization < ActiveRecord::Migration[5.1]
   def change
     add_column :bulkrax_entries, :error_class, :string unless column_exists?(:bulkrax_entries, :error_class)
     add_column :bulkrax_importers, :error_class, :string unless column_exists?(:bulkrax_importers, :error_class)
