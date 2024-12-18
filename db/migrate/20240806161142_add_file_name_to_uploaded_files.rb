9caffedd1fdcb903f4f94d94d6e1b44fea2a395b
Kirk Wang <k3wang@gmail.com>
2024-12-17 17:21:10 -0800

    Update db/migrate/20240806161142_add_file_name_to_uploaded_files.rb
    Co-authored-by: Rob Kaufman <rob@notch8.com>

--- db/migrate/20240806161142_add_file_name_to_uploaded_files.rb
+++ db/migrate/20240806161142_add_file_name_to_uploaded_files.rb
@@ -1,7 +1,5 @@
 class AddFileNameToUploadedFiles < ActiveRecord::Migration[5.2]
   def change
-    if table_exists?(:uploaded_files)
-      add_column :uploaded_files, :filename, :string unless column_exists?(:uploaded_files, :filename)
-    end
+    add_column :uploaded_files, :filename, :string if table_exists?(:uploaded_files) && !column_exists?(:uploaded_files, :filename)
   end
 end
