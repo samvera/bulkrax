44b471cf1661600a36b1926657971848feec0551
Kirk Wang <k3wang@gmail.com>
2024-12-17 21:37:19 -0800

    Add guard to uploaded_files migration (#994)
    * Add guard to uploaded_files migration
    
    This commit will check if uploaded_files is a table before adding the
    file_name column to it.  In a Hydra application, it is not so this was
    crashing.
    
    * Update db/migrate/20240806161142_add_file_name_to_uploaded_files.rb
    
    Co-authored-by: Rob Kaufman <rob@notch8.com>
    
    ---------
    
    Co-authored-by: Rob Kaufman <rob@notch8.com>

--- db/migrate/20240806161142_add_file_name_to_uploaded_files.rb
+++ db/migrate/20240806161142_add_file_name_to_uploaded_files.rb
@@ -1,5 +1,5 @@
 class AddFileNameToUploadedFiles < ActiveRecord::Migration[5.2]
   def change
-    add_column :uploaded_files, :filename, :string unless column_exists?(:uploaded_files, :filename)
+    add_column :uploaded_files, :filename, :string if table_exists?(:uploaded_files) && !column_exists?(:uploaded_files, :filename)
   end
 end
