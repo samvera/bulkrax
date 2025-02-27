182a2a4f1d3924e1fe292603836fe928053233b2
Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2025-02-26 22:31:31 -0800

    handle other possible split value types

--- app/parsers/bulkrax/csv_parser.rb
+++ app/parsers/bulkrax/csv_parser.rb
@@ -341,7 +341,15 @@ module Bulkrax
         file_mapping = Bulkrax.field_mappings.dig(self.class.to_s, 'file', :from)&.first&.to_sym || :file
         next if r[file_mapping].blank?
 
-        split_pattern = Regexp.new(Bulkrax.field_mappings.dig(self.class.to_s, :file, :split)) || Bulkrax.multi_value_element_split_on
+        split_value = Bulkrax.field_mappings.dig(self.class.to_s, :file, :split)
+        split_pattern = case split_value
+                        when Regexp
+                          split_value
+                        when String
+                          Regexp.new(split_value)
+                        else
+                          Bulkrax.multi_value_element_split_on
+                        end
         r[file_mapping].split(split_pattern).map do |f|
           file = File.join(path_to_files, f.tr(' ', '_'))
           if File.exist?(file) # rubocop:disable Style/GuardClause
