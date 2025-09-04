21460b19675f1e882b103a0b745d50f67e785abe
LaRita Robinson <larita@notch8.com>
2025-09-04 17:57:10 -0400

    Thumbnail method rename

--- app/factories/bulkrax/valkyrie_object_factory.rb
+++ app/factories/bulkrax/valkyrie_object_factory.rb
@@ -223,7 +223,7 @@ module Bulkrax
 
     ##
     # @return [Object] the thumbnail for the resource/object
-    def self.thumbnail(resource:)
+    def self.thumbnail_for(resource:)
       return nil unless resource.respond_to?(:thumbnail_id) && resource.thumbnail_id.present?
       Bulkrax.object_factory.find(resource.thumbnail_id.to_s)
     rescue Bulkrax::ObjectFactoryInterface::ObjectNotFoundError
