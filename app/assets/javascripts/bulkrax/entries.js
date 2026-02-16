246efd39af994a7749ed3ce446ddeca6cacf727f
Xavier Tilley <hippietilley@gmail.com>
2026-02-16 15:50:03 -0500

    I1078 turbolinks fallbacks (#1108)
    * add Turbolinks, Turbo, and jQuery fallbacks
    
    * forgot the else if conditional
    
    * forgot to remove a console.log

--- app/assets/javascripts/bulkrax/entries.js
+++ app/assets/javascripts/bulkrax/entries.js
@@ -1,15 +1,22 @@
-$( document ).on('turbolinks:load ready', function() {
-  
-  $( "button#entry_error" ).click(function() {
-    $( "#error_trace" ).toggle();
+function setupButtonToggles() {
+  $("button#entry_error").click(function() {
+    $("#error_trace").toggle();
   });
 
-  $( "button#raw_button" ).click(function() {
-    $( "#raw_metadata" ).toggle();
+  $("button#raw_button").click(function() {
+    $("#raw_metadata").toggle();
   });
 
-  $( "button#parsed_button" ).click(function() {
-    $( "#parsed_metadata" ).toggle();
+  $("button#parsed_button").click(function() {
+    $("#parsed_metadata").toggle();
   });
-  
-})
+}
+
+// Use Turbolinks if available, fallback to Turbo if available, fallback to vanilla JS if needed.
+if (typeof Turbolinks !== 'undefined' && Turbolinks !== null) {
+  $(document).on('turbolinks:load ready', setupButtonToggles());
+} else if (typeof Turbo !== 'undefined') {
+  $(document).on('turbo:load ready', setupButtonToggles());
+} else {
+  $(document).on('DOMContentLoaded', setupButtonToggles());
+}
