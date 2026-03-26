1b1e0942bf15815f09111a1bf5de88a03482f36c
Rob Kaufman <rob@notch8.com>
2026-03-26 13:50:35 -0700

    Change guided_import_enabled to use ActiveModel Boolean

--- lib/bulkrax.rb
+++ lib/bulkrax.rb
@@ -262,7 +262,7 @@ module Bulkrax
     conf.server_name = 'bulkrax@example.com'
     conf.relationship_job_class = "Bulkrax::CreateRelationshipsJob"
     conf.required_elements = ['title']
-    conf.guided_import_enabled = ENV.fetch('BULKRAX_GUIDED_IMPORTER', 'true').casecmp('true').zero?
+    conf.guided_import_enabled = ActiveModel::Type::Boolean.new.cast(ENV.fetch('BULKRAX_GUIDED_IMPORTER', false))
     conf.guided_import_demo_scenarios_enabled = false
 
     # Hash of Generic field_mappings for use in the view
